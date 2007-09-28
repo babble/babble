4e4691bb502658c52f1c0bbd438771fad92ceb93
Eliot Horowitz <ehorowitz@shopwiki.com>
2007-09-28 13:46:30 -0400

    more tests

---
+++ src/test/ed/js/engine/scope2.js
@@ -0,0 +1,31 @@
+
+var a = 3;
+var b = 4;
+
+function foo( a , b ){
+    var v = function(){
+        return a + b;
+    }
+    return v;
+}
+
+function foo2( a , b ){
+    var v = function(){
+        print( c );
+        return a + b;
+    }
+    a = 100;
+    return v;
+}
+
+
+
+var f = foo( a , b );
+var f2 = foo2( a , b );
+
+a = 4; b = 5;
+
+var c = 2;
+
+print( "7 = " + f() );
+print( "7 = " + f2() );
