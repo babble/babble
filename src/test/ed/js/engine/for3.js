2be42920907e40eeab05260413d9bdb8f43887f7
Eliot Horowitz <ehorowitz@shopwiki.com>
2007-10-06 13:06:27 -0400

    more tests

--- src/test/ed/js/engine/for3.js
+++ src/test/ed/js/engine/for3.js
@@ -4,3 +4,32 @@ for ( var i=0; i<2; i = i + 1 ){
     print( b );
 }
 print( i );
+
+var i = 17;
+function foo(){
+    var i = 213;
+};
+foo();
+print( i );
+
+var i = 17;
+function foo2(){
+    i = 213;
+};
+foo2();
+print( i );
+
+
+function bb( j ){
+    for ( var i=0; i<4; i = i + 1 ){
+        print( i + " " + j );
+    }
+}
+
+function a(){
+    for ( var i=0; i<4; i = i + 1 ){
+        bb( i );
+    }
+}
+
+a();
