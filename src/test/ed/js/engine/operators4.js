bb2cb74f2e04cb4d224010a8aec002d283e51e93
Eliot Horowitz <ehorowitz@shopwiki.com>
2007-10-08 09:21:28 -0400

    more tests

---
+++ src/test/ed/js/engine/operators4.js
@@ -0,0 +1,28 @@
+
+var a = 5;
+a += 1;
+print( a );
+
+b = Object();
+b.a = 1;
+b.a += 2;
+print( b.a );
+
+var c = 0;
+function foo(){
+    var d = c;
+    c = c + 1;
+    if ( d == 0 ){
+        return "a";
+    }
+    if ( d == 1 )
+        return "b";
+    if ( d == 2 )
+        return "c";
+    if ( d == 3 )
+        return "d";
+
+    return "e";
+}
+b[ foo() ] += 4;
+print( b.a );
