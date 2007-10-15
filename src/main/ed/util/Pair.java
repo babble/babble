20355b9787a1edef50eb73d0a8b21452ff60cf3b
Eliot Horowitz <ehorowitz@shopwiki.com>
2007-10-15 17:42:19 -0400

    just finishing pair class

--- src/main/ed/util/Pair.java
+++ src/main/ed/util/Pair.java
@@ -8,6 +8,29 @@ public class Pair<A,B>{
         second = b;
     }
 
+    public int hashCode(){
+        return 
+            ( first == null ? 0 : first.hashCode() ) + 
+            ( second == null ? 0 : second.hashCode() );
+    }
+
+    public boolean equals( Object o ){
+        if ( ! ( o instanceof Pair ) )
+            return false;
+
+        Pair other = (Pair)o;
+        return _equals( first , other.first ) && _equals( second , other.second );
+    }
+
+    private final boolean _equals( Object o1 , Object o2 ){
+        if ( o1 == null )
+            return o2 == null;
+        if ( o2 == null )
+            return false;
+        
+        return o1.equals( o2 );
+    }
+
     public A first;
     public B second;
 }
