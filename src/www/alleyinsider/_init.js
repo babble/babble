ee5aeb0ac87956016016ecba24cab41de21a1a38
Eliot Horowitz <erh@erh-tnt.local>
2007-11-04 16:23:26 -0500

    starting blg template

---
+++ src/www/alleyinsider/_init.js
@@ -0,0 +1,48 @@
+
+var Auth = {
+    
+    /**
+       @return null or { user , pass }
+     */
+    getUserAndPass: function( req ){
+        var auth = req.getHeader("Authorization");
+        if ( ! auth )
+            return null;
+        
+        if ( ! auth.match( /^Basic / ) )
+            return null;
+        
+        
+        auth = auth.substring( 6 );
+        auth = Base64.decode( auth );
+        var idx = auth.indexOf( ":" );
+
+        if ( idx <= 0 )
+            return null;
+        
+        return { user: auth.substring( 0 , idx ) , pass: auth.substring( idx + 1 ) };
+    }  ,
+     
+    reject: function( res , name ){
+        res.setHeader( "WWW-Authenticate" , "Basic realm=\"" + name + "\"" );
+        res.setResponseCode( 401 );
+        return "no";
+    }
+    
+};
+
+function allowed( req , res , uri ){
+    
+    if ( ! uri.match( /^\/admin/ ) )
+        return;
+    
+    var auth = Auth.getUserAndPass( req );
+    if ( auth ){
+        if ( auth.user == "abc" && auth.pass == "17" ){
+            user = auth.user;x
+            return;
+        }
+    }
+    
+    return Auth.reject( res , "Alley Insider");
+}
