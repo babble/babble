532fa2aa019170bf330a1070b386741bce0af362
Eliot Horowitz <ehorowitz@shopwiki.com>
2007-11-23 13:18:35 -0500

    wiki parsing

---
+++ src/main/ed/appserver/jxp/WikiBlock.java
@@ -0,0 +1,12 @@
+// WikiBlock.java
+
+package ed.appserver.jxp;
+
+class WikiBlock extends Block {
+
+    WikiBlock( String raw , int lineno ){
+        super( Type.WIKI , raw , lineno );
+    }
+
+
+}
