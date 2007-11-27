590b2d3da3a17ce6e12acc6ffadf7b061545c551
Eliot Horowitz <ehorowitz@shopwiki.com>
2007-11-26 23:01:59 -0500

    starting image import

--- src/main/ed/db/migrate/MT.java
+++ src/main/ed/db/migrate/MT.java
@@ -3,6 +3,8 @@
 package ed.db.migrate;
 
 import java.sql.*;
+import java.util.*;
+import java.util.regex.*;
 
 import ed.db.*;
 import ed.js.*;
@@ -18,7 +20,7 @@ public class MT {
         DBCollection coll = db.getCollection( "blog" ).getCollection( "posts" );
 
         Statement stmt = conn.createStatement();
-        ResultSet res = stmt.executeQuery( "SELECT * FROM mt_entry , mt_author WHERE entry_author_id = author_id ORDER BY entry_id DESC LIMIT 20 " );
+        ResultSet res = stmt.executeQuery( "SELECT * FROM mt_entry , mt_author WHERE entry_author_id = author_id ORDER BY entry_id DESC LIMIT 10 " );
         
         Statement commentsStmt = conn.createStatement();
         ResultSet comments = commentsStmt.executeQuery( "SELECT * FROM mt_comment WHERE comment_visible = 1 ORDER BY comment_entry_id DESC" ); 
@@ -32,10 +34,14 @@ public class MT {
         while ( res.next() ){
             System.out.println( res.getString("entry_title" ) ); 
 
+            JSObject search = new JSObjectBase();
             JSObject o = new JSObjectBase();
+
             JSDate d = new JSDate( res.getTimestamp( "entry_authored_on" ).getTime() );
 
             o.set( "name"  , d.getYear() + "/" + d.getMonth() + "/" + res.getString("entry_basename") );
+            search.set( "name" , o.get( "name" ) );
+
             o.set( "title"  , res.getString("entry_title") );
             o.set( "content" , res.getString( "entry_text" ) + "\n\n---JUMP---\n\n" + res.getString( "entry_text_more" ) );
             o.set( "author" , res.getString( "author_name" ) );
@@ -50,7 +56,6 @@ public class MT {
             JSArray ca = null;
             
             while ( moreComments && comments.getInt( "comment_entry_id" ) == res.getInt( "entry_id" ) ){
-                System.out.println( "\t found a comment" );
                 if ( ca == null ){
                     ca = new JSArray();
                     o.set( "comments" , ca );
@@ -69,6 +74,7 @@ public class MT {
             }
             
 
+            // categories
 
             catQuery.setInt( 1 , res.getInt( "entry_id" ) );
             ResultSet catRS = catQuery.executeQuery();
@@ -76,16 +82,32 @@ public class MT {
             while ( catRS.next() )
                 cats.add( catRS.getString(1) );
             catRS.close();
-            
             o.set( "categories" , cats );
-                
-            System.out.println( JSON.serialize( o ) );
-            coll.save( o );
+            
+            // images
+            doImages( o.get( "content" ).toString() );
+
+            //System.out.println( JSON.serialize( o ) );
+            coll.update( search , o , true );
         }
         
         res.close();
         stmt.close();
         
+    }
+
+    static void doImages( String content ){
+        Matcher m = Pattern.compile( "<img.*?src=['\"](.*?)['\"]" , Pattern.CASE_INSENSITIVE ).matcher( content );
+        while ( m.find() ){
+            String url = m.group(1);
+            System.out.println( url );
+            if (  url.startsWith( "/" ) )
+                url = "http://static.alleyinsider.com" + url;
+            else
+                url = url.replaceAll( "www." , "static." );
+            System.out.println( url );
+            
+        }
     }
 
     public static void main( String args[] )
