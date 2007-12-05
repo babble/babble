1410e281f469e549f2e1069b8eac663f942ffd72
Eliot Horowitz <ehorowitz@shopwiki.com>
2007-12-05 12:35:53 -0500

    captcha

---
+++ src/main/ed/js/JSCaptcha.java
@@ -0,0 +1,26 @@
+// JSCaptcha.java
+
+package ed.js;
+
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+
+import nl.captcha.servlet.*;
+
+import ed.net.httpserver.*;
+
+public class JSCaptcha {
+    
+    public synchronized void img( String s , HttpResponse response )
+        throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        _producer.createImage( out , "eliot" );
+        byte bb[] = out.toByteArray();
+        
+        response.setHeader( "Content-Type" , "image/jpeg" );
+        response.setData( ByteBuffer.wrap( bb ) );
+    }
+
+    final CaptchaProducer _producer = new DefaultCaptchaIml( new Properties() );
+}
