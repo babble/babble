3031f692dbfc50d7ae62ddaaf5e1e304ffa7e075
Yellow <yellow@10gen.com>
2007-12-12 20:25:04 +0000

    exception

--- src/main/ed/js/engine/Scope.java
+++ src/main/ed/js/engine/Scope.java
@@ -367,6 +367,12 @@ public class Scope {
                         }
                         return ret;
                     }
+		    catch ( InvocationTargetException e ){
+			throw new RuntimeException( e.getCause() );
+		    }
+		    catch ( RuntimeException e ){
+			throw e;
+		    }
                     catch ( Exception e ){
                         throw new RuntimeException( e );
                     }
