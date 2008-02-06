a7290df4643b3bfc64462f82a3082e5f527e4bc6
Eliot Horowitz <ehorowitz@shopwiki.com>
2008-02-06 10:54:32 -0500

    make test better

--- src/test/ed/db/ref1.js
+++ src/test/ed/db/ref1.js
@@ -4,15 +4,22 @@ db = connect( "tests" );
 t1 = db.table1; 
 t2 = db.table2; 
 
+t1.remove( {} );
+t2.remove( {} );
+
 o1 = {a: 'a', b: "b"}; 
 o2 = {e: 'e', o: o1}; 
 
 t1.save(o1); 
 t2.save(o2); 
 
+origo1id = o1._id;
+
 o1 = null; 
 o2 = null; 
 o2 = t2.findOne(); 
+
+print( tojson( o2 ) );
 
 assert( "a" == o2.o.a);
 assert( "b" == o2.o.b); 
@@ -20,9 +27,10 @@ assert( "b" == o2.o.b);
 o1 = o2.o; 
 o1.a = "c"; 
 
-delete o1._id; 
-delete o1._ns; 
+t2.save(o2);
 
-db.asd.save( o1 );
+assert( t2.findOne( o2._id ).o.a == "c" );
+assert( t2.findOne( o2._id ).o._id == origo1id );
+assert( t1.findOne( origo1id ).a == "c" );
 
 
