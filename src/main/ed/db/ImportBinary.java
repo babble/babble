8a72c7019fa359ab3395fa510e46db4f7c772cf2
Yellow <yellow@10gen.com>
2008-02-19 20:08:24 +0000

    checkpoint

--- src/main/ed/db/ImportBinary.java
+++ src/main/ed/db/ImportBinary.java
@@ -55,7 +55,7 @@ public class ImportBinary {
             coll.save( o );
         }
         
-        coll.find( null , null , 0 , 1 );
+        coll.find( new JSObjectBase() , null , 0 , 1 );
     }
 
     public static void main( String args[] )
