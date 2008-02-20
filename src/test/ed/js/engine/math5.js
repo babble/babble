75d47b24169be89af0bf7659055a5e6208550ed2
Ethan Glasser-Camp <ethan@Apocalypse.(none)>
2008-02-20 12:02:04 -0500

    More unit tests.

--- src/test/ed/js/engine/math5.js
+++ src/test/ed/js/engine/math5.js
@@ -15,5 +15,11 @@ print( parseInt( "AB" , 15 ) );
 print( parseInt( "--5" ) );
 print( parseInt( "-A", 12 ) );
 
+print( parseInt( "5.0" ) );
+print( parseInt( "5.ueu" ) );
+print( parseInt( "5apb" , 16 ) );
+print( parseInt( "-54o" , 55 ) );
+print( parseInt( "54o" , 55 ) );
+
 print( parseFloat( "-3.1" ) );
 print( parseInt( "asd" ) || 5 );
