9fed8161acc11c1b9a114966cc718ed74d1b6c87
Eliot Horowitz <ehorowitz@shopwiki.com>
2008-02-23 15:49:29 -0500

    test for inc

---
+++ src/test/ed/db/inc1.js
@@ -0,0 +1,15 @@
+
+db = connect( "test" );
+t = db.updateTest;
+t.remove( {} );
+
+o = { name : "foo" , z : 17 , num : 0 };
+t.save( o );
+t.ensureIndex( { name : 1 } );
+assert( t.findOne( { name : "foo" } ).z == 17 );
+
+t.update( { name : "foo" } , { $inc : { num : 1 } } );
+
+assert( t.findOne( { name : "foo" }  ) );
+assert( t.findOne( { name : "foo" } ).z == 17 );
+
