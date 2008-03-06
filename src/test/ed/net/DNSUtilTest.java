de2490f6cff431f4834687f2e27aee45796f4bd6
Eliot Horowitz <ehorowitz@shopwiki.com>
2008-03-06 10:07:19 -0500

    getSubDomain

--- src/test/ed/net/DNSUtilTest.java
+++ src/test/ed/net/DNSUtilTest.java
@@ -22,6 +22,14 @@ public class DNSUtilTest extends TestCase {
         assertEquals( "a.a" , DNSUtil.getDomain( "a.a.a.a.a" ) );
     }
 
+    public void testSubdomain(){
+        assertEquals( "foo" , DNSUtil.getSubdomain( "foo.shopwiki.com" ) );
+        assertEquals( "foo.bar" , DNSUtil.getSubdomain( "foo.bar.shopwiki.com" ) );
+        assertEquals( "www" , DNSUtil.getSubdomain( "shopwiki.com" ) );
+        assertEquals( "www" , DNSUtil.getSubdomain( "www.shopwiki.com" ) );
+        assertEquals( "www" , DNSUtil.getSubdomain( ".shopwiki.com" ) );
+    }
+
     public static void main( String args[] ){
         (new DNSUtilTest()).runConsole();
     }
