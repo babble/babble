36c86fc27263b6af861c6430d67751fd74ca8440
Ethan Glasser-Camp <ethan@Apocalypse.(none)>
2008-03-10 18:37:29 -0400

    Weird test case which we somehow pass.

--- src/test/ed/js/engine/scope6.js
+++ src/test/ed/js/engine/scope6.js
@@ -9,3 +9,20 @@ function(t) {
 };
 
 print( opt );
+
+
+increm = function(options){
+    n = options.n;
+    return function(i){
+        return i+n;
+    };
+};
+
+f = increm({n: 5});
+print(f(2));
+
+g = increm({n: 8});
+print(g(2));
+
+print(f(2));
+
