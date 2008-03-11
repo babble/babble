8ba22afed79257ef7c5120a118e2ecc5a37d4e01
Yellow <yellow@10gen.com>
2008-03-11 22:27:36 +0000

    prettier console output

--- src/main/ed/log/ConsoleAppender.java
+++ src/main/ed/log/ConsoleAppender.java
@@ -5,7 +5,7 @@ package ed.log;
 public class ConsoleAppender implements Appender {
 
     public void append( String loggerName , ed.js.JSDate date , Level level , String msg , Throwable throwable , Thread thread ){
-        System.out.println( "[" + date + "] " + loggerName + " [" + thread.getName() + "] " + level + " " + msg );
+        System.out.println( "[" + date + "] " + thread.getName() + " || " + loggerName + " " + level + " >> " + msg );
         if ( throwable != null )
             throwable.printStackTrace();
     }
