89364bb4589db281238bf510967bc058f12934ab
Eliot Horowitz <erh@ERH-OFC-MP.local>
2008-03-13 13:50:22 -0400

    exceptions

--- src/main/ed/js/JSException.java
+++ src/main/ed/js/JSException.java
@@ -2,14 +2,48 @@
 
 package ed.js;
 
+import ed.js.func.*;
+import ed.js.engine.*;
+
 public class JSException extends RuntimeException {
+
+    public final static JSFunction _quiteCons = new JSFunctionCalls1(){
+            
+            public JSObjectBase newOne(){
+                throw new JSException( "don't use new" );
+            }
+
+            public Object call( Scope scope , Object msg , Object[] extra ){
+                return new Quiet( msg );
+            }
+            
+            protected void init(){
+
+            }
+        };
+    
+
+    public final static JSFunction _cons = new JSFunctionCalls1(){
+
+            public JSObjectBase newOne(){
+                throw new JSException( "don't use new" );
+            }
+
+            public Object call( Scope scope , Object msg , Object[] extra ){
+                return new JSException( msg );
+            }
+            
+            protected void init(){
+                set( "Quiet" , _quiteCons );
+            }
+        };
 
     public JSException( Object o ){
         this( o , null );
     }
     
     public JSException( Object o , Throwable t ){
-        super( o.toString() , _fix( t ) );
+        super( ( o == null ? "" : o.toString() ).toString() , _fix( t ) );
         _object = o;
     }
 
@@ -23,5 +57,11 @@ public class JSException extends RuntimeException {
         if ( t instanceof java.lang.reflect.InvocationTargetException )
             return ((java.lang.reflect.InvocationTargetException)t).getTargetException();
         return t;
+    }
+
+    public static class Quiet extends JSException {
+        public Quiet( Object msg ){
+            super( msg );
+        }
     }
 }
