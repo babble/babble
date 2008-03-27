bac480ef6b4b1db293f541a454a80ca690ab9ad2
Eliot Horowitz <erh@ERH-OFC-MP.local>
2008-03-27 09:31:28 -0400

    better

--- src/test/ed/db/jni1.js
+++ src/test/ed/db/jni1.js
@@ -1,9 +1,9 @@
 
 
 db = connect( "foo" , "127.0.0.1" );
-t = db.t;
-
+t = db.jni1;
 t.remove( {} );
+
 t.save( { z : 1 } );
 t.save( { z : 2 } );
 assert( 2 == t.find( { $where : function(){ return 1; } } ).length() );
