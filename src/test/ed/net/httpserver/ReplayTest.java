50695f229847ed3ba7301a30d6dcbacc17a7938a
Eliot Horowitz <ehorowitz@shopwiki.com>
2008-03-30 10:36:37 -0400

    unit test fix

--- src/test/ed/net/httpserver/ReplayTest.java
+++ src/test/ed/net/httpserver/ReplayTest.java
@@ -10,15 +10,15 @@ public class ReplayTest extends TestCase {
 
     @Test(groups = {"basic"})
     public void testHeaders(){
-        Replay r = new Replay( null , 0 , "www.shopwiki.com" );
+        Replay r = new Replay( "foo" , 0 , "www.shopwiki.com" );
         
-        assertEquals( "GET /\nConnection: close\nHost: www.shopwiki.com\n\n" ,
+        assertEquals( "GET /\nConnection: close\nHost: www.shopwiki.com\nX-Replay: y\n\n" ,
                       r.fixHeaders( "GET /\nConnection: asd\nHost: asdasd\n\n" ) );
 
-        assertEquals( "GET /\nConnection: close\nHost: www.shopwiki.com\nA: B\n\n" ,
+        assertEquals( "GET /\nConnection: close\nHost: www.shopwiki.com\nA: B\nX-Replay: y\n\n" ,
                       r.fixHeaders( "GET /\nConnection: asd\nHost: asdasd\nA: B\n\n" ) );
 
-        assertEquals( "GET /\nConnection: close\nHost: www.shopwiki.com\n\n" ,
+        assertEquals( "GET /\nConnection: close\nHost: www.shopwiki.com\nX-Replay: y\n\n" ,
                       r.fixHeaders( "GET /\nConnection: asd\n\n" ) );
     }
 
