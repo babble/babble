b6e16afcbe01c06c656e76ed1270191708d78254
Eliot Horowitz <erh@ERH-OFC-MP.local>
2008-04-03 18:40:23 -0400

    better

--- src/main/ed/cloud/site1.js
+++ src/main/ed/cloud/site1.js
@@ -1,18 +1,46 @@
 // site1.js
 
 
-Cloud.Instance = function(){
+Cloud.Environment = function(){
     this.branch = null;
     this.name = this.branch;
     this.db = null;
     this.pool = null;
+    this.iid = ObjectId();
 };
 
 Cloud.Site = function( name ){
     this.name = name;
     this.created = new Date();
-    this.instances = [];
-    this.instances._dbCons = Cloud.Instance;
+    this.environments = [];
+    this.environments._dbCons = Cloud.Environment;
+};
+
+Cloud.Site.prototype.findEnvironmentByName = function( name ){
+    if ( ! name )
+        return null;
+    
+    var ret = null;
+    this.environments.forEach( function(z){
+        if ( z.name == name )
+            ret = z;
+    } );
+    return ret;
+};
+
+Cloud.Site.prototype.findEnvironmentById = function( iid ){
+    if ( ! iid )
+        return null;
+    
+    if ( isString( iid ) )
+        iid = ObjectId( iid );
+    
+    var ret = null;
+    this.environments.forEach( function(z){
+        if ( z.iid == iid )
+            ret = z;
+    } );
+    return ret;
 };
 
 Cloud.Site.forName = function( name , create ){
