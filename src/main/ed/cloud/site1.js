1dda89525480910eb2bb47873f2f924afe096687
Eliot Horowitz <erh@ERH-OFC-MP.local>
2008-04-11 15:11:37 -0400

    dbs

--- src/main/ed/cloud/site1.js
+++ src/main/ed/cloud/site1.js
@@ -9,11 +9,21 @@ Cloud.Environment = function(){
     this.iid = ObjectId();
 };
 
+Cloud.DB = function(){
+    this.name = null;
+    this.server = null;
+    this.did = ObjectId();
+};
+
 Cloud.Site = function( name ){
     this.name = name;
     this.created = new Date();
+    
     this.environments = [];
     this.environments._dbCons = Cloud.Environment;
+
+    this.dbs = [];
+    this.dbs._dbCons = Cloud.DB;
 };
 
 Cloud.Site.prototype.findEnvironmentByName = function( name ){
@@ -42,6 +52,34 @@ Cloud.Site.prototype.findEnvironmentById = function( iid ){
     } );
     return ret;
 };
+
+Cloud.Site.prototype.findDBByName = function( name ){
+    if ( ! name )
+        return null;
+    
+    var ret = null;
+    this.dbs.forEach( function(z){
+        if ( z.name == name )
+            ret = z;
+    } );
+    return ret;
+};
+
+Cloud.Site.prototype.findDBById = function( did ){
+    if ( ! did )
+        return null;
+    
+    if ( isString( did ) )
+        did = ObjectId( did );
+    
+    var ret = null;
+    this.dbs.forEach( function(z){
+        if ( z.did == did )
+            ret = z;
+    } );
+    return ret;
+};
+
 
 Cloud.Site.forName = function( name , create ){
     var s = db.sites.findOne( { name : name } );
