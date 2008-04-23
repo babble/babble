91a21c7345c6f85abdb6a63e32d7719a2f2157bb
Eliot Horowitz <erh@ERH-OFC-MP.local>
2008-04-23 18:00:48 -0400

    start of a djang10 template engine

---
+++ src/main/ed/appserver/templates/Djang10Converter.java
@@ -0,0 +1,58 @@
+// Djang10Converter.java
+
+package ed.appserver.templates;
+
+import java.util.*;
+
+import ed.util.*;
+
+public class Djang10Converter extends HtmlLikeConverter {
+
+    public Djang10Converter(){
+        super( _codeTags );
+    }
+
+    protected boolean wants( Template t ){
+        return t.getName().endsWith( ".djang10" );
+    }
+
+    protected String getNewName( Template t ){
+        return t.getName().replace( "\\.djang10" , "_djang10.js" );
+    }
+
+    protected void gotCode( Generator g , CodeMarker cm , String code ){
+        if ( cm._startTag.equals( "{{" ) ){
+            g.append( "print( " );
+            g.append( code );
+            g.append( " );\n" );
+            return;
+        }
+
+        throw new RuntimeException( "can't handle : " + cm._startTag );
+    }
+
+    protected void gotStartTag( Generator g , String tag , String restOfTag ){
+        gotText( g , "<" + tag + " " + restOfTag + ">" );
+    }
+    
+    protected void gotEndTag( Generator g , String tag ){
+        gotText( g , "</" + tag + ">" );
+    }
+    
+    protected void gotText( Generator g , String text ){
+        String lines[] = text.split( "[\r\n]+" );
+        for ( String line : lines ){
+            line = StringUtil.replace( line , "\\" , "\\\\" );
+            line = StringUtil.replace( line , "\"" , "\\\"" );
+            g.append( "print( \"" + line + "\" );\n" );
+        }
+    }
+
+    static List<CodeMarker> _codeTags = new ArrayList<CodeMarker>();
+    static {
+        _codeTags.add( new CodeMarker( "{{" , "}}" ) );
+        //_codeTags.add( new CodeMarker( "{" , "}" ) );
+
+    }
+
+}
