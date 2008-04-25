50e9edbaf3416cae75cdae2e4fb30a8f11cc913d
Eliot Horowitz <erh@ERH-OFC-MP.local>
2008-04-25 15:39:07 -0400

    reset context

--- src/main/ed/appserver/AppServer.java
+++ src/main/ed/appserver/AppServer.java
@@ -18,16 +18,8 @@ public class AppServer implements HttpHandler {
     static String OUR_DOMAINS[] = new String[]{ ".latenightcoders.com" , ".10gen.com" };
     static String CDN_HOST[] = new String[]{ "origin." , "origin-local." , "static." , "static-local." , "secure." };
 
-    public AppServer( AppContext defaultContext ){
-        this( defaultContext , null );
-    }
-    
-    public AppServer( String defaultContext , String root ){
-        this( new AppContext( defaultContext ) , root );
-    }
-
-    public AppServer( AppContext defaultContext , String root ){
-        _defaultContext = defaultContext;
+    public AppServer( String defaultWebRoot , String root ){
+        _defaultWebRoot = defaultWebRoot;
         _root = root;
         _rootFile = _root == null ? null : new File( _root );
     }
@@ -41,7 +33,7 @@ public class AppServer implements HttpHandler {
 
         if ( host == null || _root == null || host.length() == 0 ){
             if ( D ) System.out.println( " using default context for [" + host + "]" );
-            return _defaultContext;
+            return _getDefaultContext();
         }
 
         AppContext ac = _context.get( host );
@@ -112,7 +104,7 @@ public class AppServer implements HttpHandler {
                 return getFinalContext( temp , host , useHost );
         }
 
-        return _defaultContext;
+        return _getDefaultContext();
     }
 
     AppContext getFinalContext( File f , String host , String useHost ){
@@ -421,8 +413,31 @@ public class AppServer implements HttpHandler {
         return 10000;
     }
 
+    private AppContext _getContextFromMap( String host ){
+        AppContext ac = _context.get( host );
+        if ( ac == null )
+            return null;
+        
+        if ( ! ac._reset )
+            return ac;
+
+        _context.put( host , null );
+        return null;
+    }
     
-    private final AppContext _defaultContext;
+    private synchronized AppContext _getDefaultContext(){
+        if ( _defaultContext != null && _defaultContext._reset )
+            _defaultContext = null;
+        
+        if ( _defaultContext != null )
+            return _defaultContext;
+        
+        _defaultContext = new AppContext( _defaultWebRoot );
+        return _defaultContext;
+    }
+
+    private final String _defaultWebRoot;
+    private AppContext _defaultContext;
     private final AppContext _coreContext = new AppContext( CoreJS.getDefaultRoot() );
     private final String _root;
     private final File _rootFile;
@@ -431,16 +446,12 @@ public class AppServer implements HttpHandler {
     public static void main( String args[] )
         throws Exception {
 
-        //System.setOut( Shell._myPrintStream );
-        //System.setErr( Shell._myPrintStream );
         
         String root = "/data/sites/admin/";
         if ( args != null && args.length > 0 ) 
             root = args[0];
 
-        AppContext ac = new AppContext( root );
-
-        AppServer as = new AppServer( ac , "/data/sites/" );
+        AppServer as = new AppServer( root , "/data/sites/" );
         
         HttpServer.addGlobalHandler( as );
         
