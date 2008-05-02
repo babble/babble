2eb00ffb760119e449da8ce2111cae20f7db2f5e
Eliot Horowitz <erh@erh-tnt.local>
2008-05-01 22:26:18 -0400

    some basic tests for JSFileLibrary

---
+++ src/test/ed/appserver/JSFileLibraryTest.java
@@ -0,0 +1,50 @@
+// JSFileLibraryTest.java
+
+package ed.appserver;
+
+import java.io.*;
+
+import org.testng.annotations.Test;
+
+import ed.*;
+import ed.js.*;
+import ed.js.engine.*;
+
+public class JSFileLibraryTest extends ed.TestCase {
+    
+    public JSFileLibraryTest(){
+        scope = Scope.GLOBAL.child();
+        root = new File( "src/test/samplewww" );
+        local = new JSFileLibrary( root , "local" , scope );
+    }
+
+    @Test(groups = {"basic"})
+    public void testSetup(){
+        assertTrue( root.exists() );
+    }
+
+    @Test(groups = {"basic"})
+    public void testBasic(){
+        assertTrue( local.get( "base" ) instanceof JSFunction );
+    }
+
+    @Test(groups = {"basic"})
+    public void testConflict(){
+        try {
+            assertTrue( local.get( "libtest" ) instanceof JSFileLibrary );
+            ((JSFileLibrary)local.get( "libtest" )).get( "a" );
+            assertTrue( false );
+        }
+        catch ( Throwable t ){
+            assertTrue( t.toString().indexOf( "collision" ) > 0 );
+        }
+    }
+
+    final Scope scope;
+    final File root;
+    final JSFileLibrary local;
+
+    public static void main( String args[] ){
+        (new JSFileLibraryTest()).runConsole();
+    }
+}
