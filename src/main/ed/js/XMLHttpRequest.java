42ca653cc03a6fd49d3ad939f1e1f02e054ce9ce
Eliot Horowitz <erh@ERH-OFC-MP.local>
2008-05-29 10:19:29 -0400

    timeout fix and error handling

--- src/main/ed/js/XMLHttpRequest.java
+++ src/main/ed/js/XMLHttpRequest.java
@@ -153,19 +153,24 @@ public class XMLHttpRequest extends JSObjectBase {
         // TODO: more real http client
         
 	Socket sock = null;
+        
+        int timeout = 0;
+        if ( get( "timeout" ) != null ){
+            timeout = ((Number)get( "timeout" )).intValue();
+            if ( timeout < 50 )
+                timeout *= 1000;
+        }
 
         try {
             setStatus( OPENED );
             
             sock = new Socket();
-            if ( get( "timeout" ) != null ){
-                sock.setSoTimeout( ((Number)get( "timeout" )).intValue() );
-            }
+            sock.setSoTimeout( timeout );
             
             if ( DEBUG ) 
                 System.out.println( "connecting to [" + getHost() + ":" + getPort() + "]" );
 
-            sock.connect( new InetSocketAddress( getHost() , getPort() ) );
+            sock.connect( new InetSocketAddress( getHost() , getPort() ) , timeout );
 
             if ( isSecure() ){
                 if ( DEBUG ) System.out.println( "\t making secure" );
@@ -255,6 +260,10 @@ public class XMLHttpRequest extends JSObjectBase {
             
             if ( DEBUG ) System.out.println( buf );
             
+        }
+        catch ( IOException ioe ){
+            set( "error" , ioe );
+            return null;
         }
         finally {
 	    try {
