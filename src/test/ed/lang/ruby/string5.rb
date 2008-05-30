bb82e7cbfca18bec46ff1b2c5b3736db2c0433c6
Eliot Horowitz <ehorowitz@shopwiki.com>
2008-05-29 22:07:48 -0400

    another test

--- src/test/ed/lang/ruby/string5.rb
+++ src/test/ed/lang/ruby/string5.rb
@@ -6,16 +6,15 @@ puts characters[5]
 puts characters[5..5]
 puts characters[5..6]
 
-# def random_token
-#   characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
-#   temp_token = ''
-#   TOKEN_LENGTH.times do
-#     pos = 5#rand(characters.length)
-#     puts pos
-#     puts characters[5..5]
-#     temp_token += characters[pos]
-#   end
-#   temp_token
-# end
+def random_token
+  characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+    temp_token = ''
+  TOKEN_LENGTH.times do
+    pos = 5
+    puts pos
+    temp_token += characters[pos..pos]
+  end
+  temp_token
+end
 
-# puts random_token
+puts random_token
