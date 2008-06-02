fa58a4f0384c721199863c868e3b03f242913a28
Eliot Horowitz <eliot@10gen.com>
2008-06-02 09:56:02 -0400

    huh?

--- src/main/ed/lang/ruby/RubyConvert.java
+++ src/main/ed/lang/ruby/RubyConvert.java
@@ -1237,15 +1237,9 @@ public class RubyConvert extends ed.MyAsserts {
             
             
             if ( cn.getSuperNode() != null ){
-                _append( "\n" , cn );
-                
-                _append( name + ".__proto__ = " , cn );
+                _append( "\n" + name + ".prototype = new " , cn );
                 _add( cn.getSuperNode() , state );
-                _append( ";\n" , cn );
-
-                _append( name + ".prototype.__proto__ = " , cn );
-                _add( cn.getSuperNode() , state );
-                _append( ".prototype;\n" , cn );
+                _append( "();\n" , cn );
             }
         }
         
