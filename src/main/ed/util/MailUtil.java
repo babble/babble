29b7fb5537e86ab926e16a8cd946760b4be4deaf
Eliot Horowitz <eliot@10gen.com>
2008-06-10 11:08:00 -0400

    java mail api is pront to errors were using only 1 session object

--- src/main/ed/util/MailUtil.java
+++ src/main/ed/util/MailUtil.java
@@ -10,7 +10,7 @@ public class MailUtil {
 
     public static Session createSession( final Properties props , final String user , final String pass ){
 
-        return Session.getDefaultInstance( props , new javax.mail.Authenticator() {
+        return Session.getInstance( props , new javax.mail.Authenticator() {
                 protected PasswordAuthentication getPasswordAuthentication() {
                     return new PasswordAuthentication( user , pass );
                 }
