26695cb158e18783d875a4601771a21a440220bf
Eliot Horowitz <eliot@10gen.com>
2008-06-13 17:56:59 -0400

    more tests that work

--- src/test/ed/js/cache1.js
+++ src/test/ed/js/cache1.js
@@ -17,4 +17,32 @@ assert( 2 == stupid.inc );
 foo.cache( 100000 , { name : "asd" } );
 assert( 2 == stupid.inc );
 
+foo.cache( 100000 , "nuts" );
+assert( 3 == stupid.inc );
+foo.cache( 100000 , "nuts" );
+assert( 3 == stupid.inc );
+
+
+
+foo.cache( 100000 , { name : "asd" , foo : 1 } );
+assert( 4 == stupid.inc );
+foo.cache( 100000 , { name : "asd" , foo : 1 } );
+assert( 4 == stupid.inc );
+
+foo.cache( 100000 , { name : "asd" , foo : 2 } );
+assert( 5 == stupid.inc );
+foo.cache( 100000 , { name : "asd" , foo : 2 } );
+assert( 5 == stupid.inc );
+
+
+foo.cache( 100000 , { name : "asd" , foo : { z : 1 } } );
+assert( 6 == stupid.inc );
+foo.cache( 100000 , { name : "asd" , foo : { z : 1 } } );
+assert( 6 == stupid.inc );
+
+foo.cache( 100000 , { name : "asd" , foo : { z : 2 } } );
+assert( 7 == stupid.inc );
+foo.cache( 100000 , { name : "asd" , foo : { z : 2 } } );
+assert( 7 == stupid.inc );
+
 
