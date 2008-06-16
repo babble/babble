aba5413ad9f886bd3f2ce9cd4bafe22071af1b6f
Eliot Horowitz <eliot@10gen.com>
2008-06-16 17:04:19 -0400

    forgot one

--- src/test/ed/js/sysexec1.js
+++ src/test/ed/js/sysexec1.js
@@ -3,4 +3,4 @@ assert( sysexec( "/bin/ls -la" ).out );
 assert( sysexec( "/bin/ls -la" ).err != null );
 assert( sysexec( "/bin/ls -la" ).out.match( /\.\./ ) );
 
-assert( sysexec( "/bin/ls" ).out != sysexec( "ls" , null , null  , "src" ) );
+assert( sysexec( "/bin/ls" ).out != sysexec( "/bin/ls" , null , null  , "src" ) );
