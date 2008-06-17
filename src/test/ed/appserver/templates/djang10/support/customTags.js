1c6bdfe7e315ec5f7160a72ccfe5211dbd38f71b
Igor Bernstein <igor@10gen.com>
2008-06-16 20:08:57 -0400

    unit test fixes

--- src/test/ed/appserver/templates/djang10/support/customTags.js
+++ src/test/ed/appserver/templates/djang10/support/customTags.js
@@ -1,29 +1,36 @@
 
-//register = new core.templates.djang10.Library();
-var djang10 = __djang10.publicApi;
 register = new djang10.Library();
 
 register.filter("moo", function() {
     return "moo";
 });
 
+var MyTagNode = function() { };
+MyTagNode.prototype = {
+    __proto__: djang10.Node.prototype,
+    __render: function(context, printer) {
+        printer("moo");
+    }
+}
 
 register.tag("myTag", function(parser, token) {
-    return {
-        render: function(cxt) {
-            return "moo";
-        }
-    }
+    return new MyTagNode();
 });
 
 
+var MyTagWithChildNode = function(children) {
+    this.children = children;
+};
+MyTagWithChildNode.prototype = {
+    __proto__: djang10.Node.prototype,
+    __render: function(context, printer) {
+        printer("before_" + this.children.render(context) + "_after");
+    }
+};
+
 register.tag("myTagWithChild", function(parser, token) {
     var children = parser.parse(["endmyTagWithChild"]);
-    parser.next_token();
+    parser.delete_first_token();
     
-    return {
-        render: function(ctx) {
-            return "before_" + children.render(ctx) + "_after";
-        }
-    }
+    return new MyTagWithChildNode(children);
 });
