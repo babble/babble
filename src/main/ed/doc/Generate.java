0f916caf403082180fbc9051ac267992f4b35a8a
kristina <k@wooster.earthlink.net>
2008-06-19 17:33:28 -0400

    javadoc and jsdoc working from corejs doc util

--- src/main/ed/doc/Generate.java
+++ src/main/ed/doc/Generate.java
@@ -1,20 +1,31 @@
 package ed.doc;
 
 import java.io.*;
+import java.util.Calendar;
 
 import ed.js.*;
 import ed.js.func.*;
 import ed.js.engine.*;
+import ed.db.*;
 import static ed.js.JSInternalFunctions.*;
 
-
+/** Documentation generator for JavaScript and Java
+ * @expose
+ */
 public class Generate {
-    public static void toHTML(String objStr) {
 
+    /** Takes objects from the db and makes them into HTML pages
+     * @expose
+     */
+    public static void toHTML(String objStr) {
+        toHTML(objStr, "../../www/html/doc/");
+    }
+
+    public static void toHTML(String objStr, String path) {
         try {
-            Process p = Runtime.getRuntime().exec(new String[]{"java", "-jar", "jsrun.jar", "app/run.js", "-d=../../../corejs/admin/doc/", "-t=templates/jsdoc2"},
+            Process p = Runtime.getRuntime().exec(new String[]{"java", "-jar", "jsrun.jar", "app/run.js", "-d="+path, "-t=templates/jsdoc2"},
                                                   null,
-                                                  new File("../external/jsdoc-toolkit/current/")
+                                                  new File("../core-modules/docgen/")
                                                   );
             BufferedWriter in = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
             in.write(objStr);
@@ -40,9 +51,85 @@ public class Generate {
         catch(Exception e) {
             e.printStackTrace();
         }
+
     }
 
-    public static void toDB(String corejs) {
 
+    public static void JSToDb(String path) {
+        try {
+            Process p = Runtime.getRuntime().exec(new String[]{"java", "-jar", "jsrun.jar", "app/run.js", "-r", "-d=../../www/html/doc", "-o=../../www/html/doc/log", "-t=templates/json", "../../corejs/"},
+                                                  null,
+                                                  new File("../core-modules/docgen/")
+                                                  );
+            BufferedWriter in = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+            in.close();
+
+            BufferedReader out = new BufferedReader(new InputStreamReader( p.getInputStream()));
+            String line2 = "";
+            StringBuffer jsdoc = new StringBuffer();
+            while ((line2 = out.readLine()) != null){
+                jsdoc.append(line2);
+            }
+
+            BufferedReader err = new BufferedReader(new InputStreamReader( p.getErrorStream()));
+            String line;
+            while ((line = err.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            p.waitFor();
+            System.out.println("exit: "+p.exitValue());
+
+            JSObjectBase ss = new JSObjectBase();
+            ss.set("symbolSet", jsdoc.toString());
+            JSObjectBase obj = new JSObjectBase();
+            obj.set("ts", Calendar.getInstance().getTime().toString());
+            obj.set("_index", ss);
+
+            DBApiLayer db = DBProvider.get("admin", "127.0.0.1", 27017);
+            DBCollection collection = db.getCollection("doc");
+            collection.save(obj);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void JavadocToDb(String path) {
+        path = "../"+path;
+        try {
+            Process p = Runtime.getRuntime().exec(new String[]{"javadoc",
+                                                               "-doclet",
+                                                               "JavadocToDB",
+                                                               "-docletpath",
+                                                               "./", path},
+                                                  null,
+                                                  new File(".")
+                                                  );
+
+            BufferedReader out = new BufferedReader(new InputStreamReader( p.getInputStream()));
+            String line2 = "";
+            StringBuffer jsdoc = new StringBuffer();
+            while ((line2 = out.readLine()) != null){
+                System.out.println(line2);
+            }
+
+            BufferedReader err = new BufferedReader(new InputStreamReader( p.getErrorStream()));
+            String line;
+            while ((line = err.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            p.waitFor();
+            System.out.println("exit: "+p.exitValue());
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void JavadocArgHelper(String arg) {
+        arg = "../"+arg;
+        com.sun.tools.javadoc.Main.execute(new String[]{"-doclet", "JavadocToDB", "-docletpath", "./", arg } );
     }
 }
---
+++ src/main/ed/doc/JavadocToDB.java
@@ -0,0 +1,221 @@
+import com.sun.javadoc.*;
+import com.sun.tools.doclets.formats.html.*;
+import java.util.*;
+import ed.js.*;
+import ed.db.*;
+
+public class JavadocToDB {
+
+	public static void getTags(Doc from, JSObjectBase dest) {
+        Tag cTags[] = from.tags();
+        for(int j=0; j<cTags.length; j++) {
+        	dest.set(cTags[j].name().substring(1), cTags[j].text());
+        }
+	}
+
+	public static JSObjectBase getMethod(MethodDoc m) {
+        JSObjectBase tempMethod = new JSObjectBase();
+        tempMethod.set("desc", m.commentText());
+        tempMethod.set("name", m.name());
+        tempMethod.set("alias", m.name());
+        tempMethod.set("isStatic", m.isStatic());
+        tempMethod.set("isAbstract", m.isAbstract());
+        tempMethod.set("isPrivate", m.isPrivate());
+        tempMethod.set("isProtected", m.isProtected());
+        tempMethod.set("isPublic", m.isPublic());
+        tempMethod.set("isa", "FUNCTION");
+        tempMethod.set("memberOf", m.containingClass().name());
+        tempMethod.set("type", m.returnType().typeName());
+        tempMethod.set("example", new JSArray());
+        tempMethod.set("exceptions", new JSArray());
+        tempMethod.set("see", new JSArray());
+        tempMethod.set("requires", new JSArray());
+
+        Tag mTags[] = m.tags();
+        for(int k=0; k<mTags.length; k++) {
+        	if(mTags[k].name().equals("@return")) {
+        		JSArray returns = new JSArray();
+        		JSObjectBase tempReturn = new JSObjectBase();
+                tempReturn.set("title", "return");
+                tempReturn.set("desc", mTags[k].text());
+                tempReturn.set("type", m.returnType().typeName());
+                returns.add(tempReturn);
+                tempMethod.set("returns", returns);
+        	}
+        	else {
+        		tempMethod.set(mTags[k].name(), mTags[k].text());
+        	}
+        }
+        if(!tempMethod.containsKey("returns"))
+        	tempMethod.set("returns", new JSArray());
+
+
+        ParamTag params[] = m.paramTags();
+        Parameter p2[] = m.parameters();
+        JSArray jsParams = new JSArray();
+        for(int k=0; k<params.length; k++) {
+            JSObjectBase tempParam = new JSObjectBase();
+            tempParam.set("title", "param");
+            tempParam.set("desc", params[k].parameterComment());
+            tempParam.set("type", p2[k].typeName());
+            tempParam.set("name", p2[k].name());
+            tempParam.set("isOptional", "false");
+            jsParams.add(tempParam);
+        }
+        tempMethod.set("_params", jsParams);
+        tempMethod.set("params", jsParams);
+        return tempMethod;
+	}
+
+	public static JSObjectBase getConstructor(ConstructorDoc m) {
+		JSObjectBase tempMethod = new JSObjectBase();
+        tempMethod.set("desc", m.commentText());
+        tempMethod.set("name", m.name());
+        tempMethod.set("alias", m.qualifiedName());
+        tempMethod.set("isStatic", m.isStatic());
+        tempMethod.set("isPrivate", m.isPrivate());
+        tempMethod.set("isProtected", m.isProtected());
+        tempMethod.set("isPublic", m.isPublic());
+        tempMethod.set("isa", "CONSTRUCTOR");
+        tempMethod.set("memberof", m.containingClass().name());
+        getTags(m, tempMethod);
+
+        ParamTag params[] = m.paramTags();
+        Parameter p2[] = m.parameters();
+        JSArray jsParams = new JSArray();
+        for(int k=0; k<params.length; k++) {
+            JSObjectBase tempParam = new JSObjectBase();
+            tempParam.set("title", "param");
+            tempParam.set("desc", params[k].parameterComment());
+            tempParam.set("type", p2[k].typeName());
+            tempParam.set("name", p2[k].name());
+            tempParam.set("isOptional", "false");
+            jsParams.add(tempParam);
+        }
+        tempMethod.set("_params", jsParams);
+        return tempMethod;
+	}
+
+    public static JSObjectBase getClasses(ClassDoc c) {
+        JSObjectBase temp = new JSObjectBase();
+
+        // get inner classes
+        ClassDoc inner[] = c.innerClasses();
+        JSArray jsInner = new JSArray();
+        for(int j=0; j<inner.length; j++) {
+            Tag[] tags = inner[j].tags("expose");
+            if(tags.length == 0) continue;
+
+            jsInner.add(getClasses(inner[j]));
+        }
+        temp.set("innerClasses", jsInner);
+
+        // get interface names
+        ClassDoc extend[] = c.interfaces();
+        JSArray jsExtends = new JSArray();
+        for(int j=0; j<extend.length; j++) {
+        	jsExtends.add(extend[j].qualifiedName());
+        }
+        temp.set("augments", jsExtends);
+
+        // get constructors
+        ConstructorDoc cons[] = c.constructors();
+        JSArray jsCons = new JSArray();
+        for(int j=0; j<cons.length; j++) {
+        	jsCons.add(getConstructor(cons[j]));
+        }
+        temp.set("constructors", jsCons);
+
+        // get properties
+        FieldDoc fields[] = c.fields();
+        JSArray jsFields = new JSArray();
+        for(int j=0; j<fields.length; j++) {
+            Tag[] mtags = fields[j].tags("expose");
+            if(mtags.length == 0) continue;
+
+            JSObjectBase tempField = new JSObjectBase();
+            tempField.set("desc", fields[j].commentText());
+            tempField.set("name", fields[j].name());
+            tempField.set("alias", fields[j].qualifiedName());
+            tempField.set("isStatic", fields[j].isStatic());
+            tempField.set("isPrivate", fields[j].isPrivate());
+            tempField.set("isProtected", fields[j].isProtected());
+            tempField.set("isPublic", fields[j].isPublic());
+            getTags(fields[j], tempField);
+            tempField.set("isa", "FUNCTION");
+            tempField.set("memberof", c.name());
+            jsFields.add(tempField);
+        }
+        temp.set("properties", jsFields);
+
+        // get methods
+        MethodDoc methods[] = c.methods();
+        JSArray jsMethods = new JSArray();
+        for (int j = 0; j < methods.length; j++) {
+            Tag[] mtags = methods[j].tags("expose");
+            if(mtags.length == 0) continue;
+
+            jsMethods.add(getMethod(methods[j]));
+        }
+        temp.set("methods", jsMethods);
+
+
+        temp.set("classDesc", c.commentText());
+        temp.set("desc", c.commentText());
+        temp.set("alias", c.name());
+        temp.set("name", c.qualifiedName());
+        temp.set("isStatic", c.isStatic());
+        temp.set("isAbstract", c.isAbstract());
+        temp.set("isPrivate", c.isPrivate());
+        temp.set("isProtected", c.isProtected());
+        temp.set("isPublic", c.isPublic());
+        temp.set("isExternalizable", c.isExternalizable());
+        temp.set("isSerializable", c.isSerializable());
+        temp.set("isInterface", c.isInterface());
+        temp.set("isa", "CONSTRUCTOR");
+        temp.set("memberof", c.name());
+        temp.set("comment", new JSObjectBase());
+        temp.set("inheritsFrom", new JSArray());
+        temp.set("example", new JSArray());
+        temp.set("params", new JSArray());
+        temp.set("exceptions", new JSArray());
+        temp.set("returns", new JSArray());
+        temp.set("requires", new JSArray());
+        temp.set("see", new JSArray());
+        getTags(c, temp);
+
+        return temp;
+    }
+
+    public static boolean start(RootDoc root) {
+
+        DBApiLayer db = DBProvider.get("admin", "127.0.0.1", 27017);
+        DBCollection collection = db.getCollection("doc");
+
+        ClassDoc[] classes = root.classes();
+
+        for (int i = 0; i < classes.length; i++) {
+            Tag[] tags = classes[i].tags("expose");
+            if(tags.length == 0) continue;
+
+            JSObjectBase obj = new JSObjectBase();
+            JSObjectBase jsClasses = new JSObjectBase();
+
+            jsClasses.set(classes[i].name(), getClasses(classes[i]));
+            jsClasses.set("fromJava", true);
+            obj.set("symbolSet", jsClasses);
+
+            JSObjectBase topLevel = new JSObjectBase();
+            topLevel.set("_index", obj);
+            topLevel.set("ts", Calendar.getInstance().getTime().toString());
+            collection.save((JSObject)topLevel);
+        }
+
+        System.out.println("done!");
+
+        // Generates standard javadoc html page
+        //return com.sun.tools.doclets.formats.html.HtmlDoclet.start(root);
+        return true;
+    }
+
+}
