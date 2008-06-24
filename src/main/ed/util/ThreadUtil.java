d3c415582dcb0a0f68c9a615018ec5a69da5b9a7
Eliot Horowitz <eliot@10gen.com>
2008-06-24 14:25:42 -0400

    printStackTrace

--- src/main/ed/util/ThreadUtil.java
+++ src/main/ed/util/ThreadUtil.java
@@ -4,6 +4,12 @@ package ed.util;
 
 public class ThreadUtil {
 
+    public static void printStackTrace(){
+        Exception e = new Exception();
+        e.fillInStackTrace();
+        e.printStackTrace();
+    }
+
     public static void sleep( long time ){
         try {
             Thread.sleep( time );
