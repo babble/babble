6baa712c0691c8b8a8681082406ab096a51be898
Geir Magnusson Jr <geir@pthbbbbbb-2.local>
2008-07-07 20:24:20 -0400

    remove comments on asserts to make this a real test now that dwight has the fix in

--- src/test/ed/db/index_check1.js
+++ src/test/ed/db/index_check1.js
@@ -2,26 +2,24 @@ db = connect("ed_db_index_check1");
 
 db.somecollection.drop();
 
-//assert(db.system.namespaces.find({name:/somecollection/}).length() == 0);
+assert(db.system.namespaces.find({name:/somecollection/}).length() == 0);
 
 db.somecollection.save({a:1});
 
-//assert(db.system.namespaces.find({name:/somecollection/}).length() == 1);
+assert(db.system.namespaces.find({name:/somecollection/}).length() == 1);
 
 db.somecollection.ensureIndex({a:1});
 
-//assert(db.system.namespaces.find({name:/somecollection/}).length() == 2);
+assert(db.system.namespaces.find({name:/somecollection/}).length() == 2);
 
 db.somecollection.drop();
 
-//assert(db.system.namespaces.find({name:/somecollection/}).length() == 0);
+assert(db.system.namespaces.find({name:/somecollection/}).length() == 0);
 
 db.somecollection.save({a:1});
 
-//assert(db.system.namespaces.find({name:/somecollection/}).length() == 1);
+assert(db.system.namespaces.find({name:/somecollection/}).length() == 1);
 
 db.somecollection.ensureIndex({a:1});
 
-//assert(db.system.namespaces.find({name:/somecollection/}).length() == 2);
-
-print("ed/db/index_check1 : please remember to uncomment my asserts!");
+assert(db.system.namespaces.find({name:/somecollection/}).length() == 2);
