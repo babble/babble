882c1e6d8a6794f9c606d5110c0138fac4ab096e
Eliot Horowitz <eliot@10gen.com>
2008-07-17 15:43:22 -0400

    range test 1 test commented out that doesn't work yet

---
+++ src/test/ed/db/range1.js
@@ -0,0 +1,21 @@
+
+db = connect( "test" );
+t = db.range1;
+
+t.drop();
+
+for ( i=1; i<=10; i++ )
+    t.save( { num : i } );
+
+t.ensureIndex( { num : 1 } );
+
+assert( t.find().sort( { num : 1 } ).toArray()[0].num == 1 );
+assert( t.find().sort( { num : -1 } ).toArray()[0].num == 10 );
+
+assert( 10 == t.find().toArray().length );
+assert( 5 == t.find( { num : { $gt : 5 } } ).toArray().length );
+assert( 4 == t.find( { num : { $lt : 5 } } ).toArray().length );
+
+// we should make this work at some point
+//assert( 1 == t.find( { num : { $gt : 5 , $lt : 7 } } ).toArray().length );
+
