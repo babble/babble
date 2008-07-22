5dc528d9aa3869fde7aca46a6921ea6c1d44425f
Eliot Horowitz <eliot@10gen.com>
2008-07-22 13:08:39 -0400

    got it backwards

--- src/main/ed/cloud/zeus_resolve.js
+++ src/main/ed/cloud/zeus_resolve.js
@@ -65,7 +65,7 @@ Cloud.Zeus.resolveTS = function(){
                     var alias = e.aliases[aliasNumber];
                     var crap = oneEnv.replace( /!branch/g , alias );
                     crap = crap.replace( /!pool/g , e.pool );
-                    crap = crap.replace( /!extra/g , "http.setHeader( \"X-Host\" , http.getHostHeader() ); \n http.setHeader( \"Host\" , string.replace( http.getHostHeader() , \"" +  alias  + "\" , \"" + e.name  + "\" ) );" );
+                    crap = crap.replace( /!extra/g , "http.setHeader( \"X-Host\" , string.replace( http.getHostHeader() , \"" +  alias  + "\" , \"" + e.name  + "\" ) );" );
 
                     envs += crap;
                 }
