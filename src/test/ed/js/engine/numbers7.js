9124382da5ac0b5533f4f50a65744dbab2d19bc5
Eliot Horowitz <eliot@10gen.com>
2008-07-23 16:06:13 -0400

    some Number fixes not perfect yet

--- src/test/ed/js/engine/numbers7.js
+++ src/test/ed/js/engine/numbers7.js
@@ -4,3 +4,12 @@ print( a );
 print( a == 5 );
 
 print( ( new Number( new Date( 12312312 ) ) ) );
+
+n = 5;
+Number.prototype.a = function(){ return 6; };
+n.a();
+
+// TODO
+//n = 5;
+//Number.prototype.b = function(){ return 7; };
+//print( n.b );
