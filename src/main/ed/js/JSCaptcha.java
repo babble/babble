c14dd31e87592de23a3eead03c4fd37825c3af32
kristina <k@wooster.earthlink.net>
2008-07-23 16:59:14 -0400

    unexposed JSCaptcha

--- src/main/ed/js/JSCaptcha.java
+++ src/main/ed/js/JSCaptcha.java
@@ -2,16 +2,16 @@
 
 /**
 *    Copyright (C) 2008 10gen Inc.
-*  
+*
 *    This program is free software: you can redistribute it and/or  modify
 *    it under the terms of the GNU Affero General Public License, version 3,
 *    as published by the Free Software Foundation.
-*  
+*
 *    This program is distributed in the hope that it will be useful,
 *    but WITHOUT ANY WARRANTY; without even the implied warranty of
 *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *    GNU Affero General Public License for more details.
-*  
+*
 *    You should have received a copy of the GNU Affero General Public License
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -39,8 +39,6 @@ import com.sun.image.codec.jpeg.*;
 
 import ed.net.httpserver.*;
 
-/** @expose
- */
 public class JSCaptcha {
 
     /** Creates a captcha image from a string and adds it to the HTTP response.
