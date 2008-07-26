629e9c79b4fc1e1d7e69459435fa87ea28241670
Eliot Horowitz <ehorowitz@shopwiki.com>
2008-07-26 15:09:27 -0400

    more debugging

--- src/test/ed/js/engine/enum1.js
+++ src/test/ed/js/engine/enum1.js
@@ -6,12 +6,17 @@ for(var key in Foo.prototype){ print(key); }
 A = { a : 1 , prototype : { b : 2 } };
 for(var key in A){ print(key); }
 
+print( "POS1" );
+
 function Foo(){}
 Foo.prototype.a = 1;
 Foo.b = 2;
 for(var key in Foo){ print(key); }
+print( "POS2" );
 for(var key in Foo.prototype){ print(key); }
+print( "POS3" );
 for(var key in (new Foo())){ print(key); }
+print( "POS4" );
 
 function Bar(){}
 Bar.__proto__ = Foo;
