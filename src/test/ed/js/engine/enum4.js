fb35f2ab485f87d978cda2be953ced8a39bfc547
Eliot Horowitz <ehorowitz@shopwiki.com>
2008-07-27 16:39:03 -0400

    debugging for crazy hudson problem

--- src/test/ed/js/engine/enum4.js
+++ src/test/ed/js/engine/enum4.js
@@ -2,11 +2,11 @@
 function Foo(){}
 Foo.prototype.a = 1;
 Foo.b = 2;
-for(var key in Foo){ print(key); }
-for(var key in Foo.prototype){ print(key); }
-for(var key in (new Foo())){ print(key); }
+for(var key in Foo){ print("Foo:" + key); }
+for(var key in Foo.prototype){ print("Foo.prototype:" + key); }
+for(var key in (new Foo())){ print("new Foo:" + key); }
 
 function Bar(){}
 Bar.__proto__ = Foo;
-for(var key in (new Bar())){ print(key); }
+for(var key in (new Bar())){ print("new Bar:" + key); }
 
