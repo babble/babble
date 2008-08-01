7cc6294ed96fd0b65b5ac0a8f6bcf63a8294c677
Igor Bernstein <igor@10gen.com>
2008-08-01 11:05:57 -0400

    added urlizetrunc filter

--- src/test/ed/appserver/templates/djang10/regression/DjangoRegressionTests.java
+++ src/test/ed/appserver/templates/djang10/regression/DjangoRegressionTests.java
@@ -84,7 +84,6 @@ public class DjangoRegressionTests {
         "filter-make_list0[3,4]",   //needs stringformat 
         "filter-slugify.*",
         "filter-stringformat.*",
-        "filter-urlizetrunc.*",
 
         //broken filters
         "chaining0[35]",
