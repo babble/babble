6f1cc1e5fa2f15aefccc7ed5ed7f4f1d9679bb96
Ethan Glasser-Camp <ethan@10gen.com>
2008-08-04 14:13:31 -0400

    More list tests.

--- src/test/ed/lang/python/list1.js
+++ src/test/ed/lang/python/list1.js
@@ -7,3 +7,6 @@ assert( pyL.length == 2 );
 assert( pyL.some( function(x){ return x == 1; } ) );
 assert( pyL.every( function(x){ return x > 0; } ) );
 
+var jsA = [1, 3, 5];
+
+assert( pyManipList(jsA) );
