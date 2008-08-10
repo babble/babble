7e2e51204037a2a3acac74f90abf71d462798538
Geir Magnusson Jr <geir@pobox.com>
2008-08-10 15:13:48 -0400

    update - check to see that dropping the index also propogates

--- src/test/ed/db/repl/repltest_ms_indexrepl1.js
+++ src/test/ed/db/repl/repltest_ms_indexrepl1.js
@@ -10,16 +10,17 @@
 __path__._repl( 
     function( dbm , dbs, info){
 
+    // ensure no objects or indices in collection on master or slave
+
     assert(dbm.test.count() == 0);
     assert(dbs.test.count() == 0);
-
     assert(dbm.test.getIndexes().length() == 0);
     assert(dbs.test.getIndexes().length() == 0);
 
-    dbm.test.ensureIndex({n:1});
-
-    assert(dbm.test.getIndexes().length() == 1);
+    // now create index on master and validate
     
+    dbm.test.ensureIndex({n:1});
+    assert(dbm.test.getIndexes().length() == 1);
     dbm.test.getIndexes().forEach(function(x) { 
         assert(x.key.n == 1);
     });
@@ -28,16 +29,29 @@ __path__._repl(
     
     sleep(info.replTimeMS);
     
+    // check to see if both master and slave have index
+   
     assert(dbm.test.getIndexes().length() == 1);
     assert(dbs.test.getIndexes().length() == 1);
-    
     dbm.test.getIndexes().forEach(function(x) { 
         assert(x.key.n == 1);
     });
-    
     dbs.test.getIndexes().forEach(function(x) { 
         assert(x.key.n == 1);
     });
+    
+    // now nuke the index, sleep, and validate that the drop propogated
+
+    dbm.test.dropIndexes();
+   
+    assert(dbm.test.getIndexes().length() == 0);
+   
+    print( " **** " + info.replTimeMS + "ms wait for replication***" );
+    
+    sleep(info.replTimeMS);
+
+    assert(dbm.test.getIndexes().length() == 0);
+    assert(dbs.test.getIndexes().length() == 0);
     
     assert(dbm.test.validate().valid, "master table test failed validation");
     assert(dbs.test.validate().valid, "slave table test failed validation");
