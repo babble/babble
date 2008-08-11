e1de3fc2c712d96f5d1805f9e5d28c30aec75796
Geir Magnusson Jr <geir@geir-magnusson-jrs-mac-pro.local>
2008-08-11 17:07:08 -0400

    Math : add sin(), cos(), pow() and PI

---
+++ src/test/ed/js/math1.js
@@ -0,0 +1,12 @@
+
+
+assert(Math.sin(0) == 0.0);
+assert(Math.sin(Math.PI / 2) == 1.0);
+assert(Math.sin(Math.PI * 3 / 2) == -1.0);
+
+assert(Math.cos(Math.PI) == -1.0);
+
+assert(Math.pow(10,0) == 1.0);
+assert(Math.pow(10,2) == 100);
+
+assert(Math.pow(2,2) == 4);
