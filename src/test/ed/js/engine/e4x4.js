0d63b0508b66242f2bd2d9bfd4ca526acbfb4e1a
kristina <k@wooster.earthlink.net>
2008-08-14 10:24:16 -0400

    fixed e4x test

--- src/test/ed/js/engine/e4x4.js
+++ src/test/ed/js/engine/e4x4.js
@@ -15,6 +15,7 @@
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+XML.ignoreComments = true;
 xml=<blargh><blah id="52" prop="some prop"><something id="1" snakes="in_planes">grr</something><!-- here is a comment --><else id="2">arg</else></blah><blah>2</blah><blah>3</blah><!-- and here is another comment --></blargh>;
 
 print(xml.blah[0].@*);
