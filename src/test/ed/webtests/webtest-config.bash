a45581fe37a6467a9c08831730f85f457e3cc9b4
Mike Dirolf <mike@10gen.com>
2008-08-19 11:24:48 -0400

    probably doesn't make sense to have insane values by default.

--- src/test/ed/webtests/webtest-config.bash
+++ src/test/ed/webtests/webtest-config.bash
@@ -6,5 +6,5 @@ GITROOT=~/10gen
 SITESROOT=~/10gen/sites
 
 export db_ip=127.0.0.1                # fetch from a site
-export db_port=1337
-export http_port=1338
+export db_port=27017
+export http_port=8080
