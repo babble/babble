39689d6be6c037c39718e9e34db1ee723891b530
Mike Dirolf <mike@10gen.com>
2008-08-19 09:36:41 -0400

    some changes to test scripts
    make executable. use webtest.sh instead of ant. move all config stuff from runSiteTests to webtest-config. run tests against versions pinned in SITE/test/_config.js instead of SITE/_config.js.

--- src/test/ed/webtests/webtest-config.bash
+++ src/test/ed/webtests/webtest-config.bash
@@ -1,3 +1,9 @@
-export WTPATH=~/Desktop/build         # this'll get changed
-export WTSITE=http://localhost:1338   # this should be provided by hudson
+export WTPATH=/usr/local/webtest
+export WTSITE=http://local.alleyinsider.com:8080
 
+GITROOT=~/10gen
+SITESROOT=~/10gen/sites
+
+export db_ip=127.0.0.1                # fetch from a site
+export db_port=27017
+export http_port=8080
