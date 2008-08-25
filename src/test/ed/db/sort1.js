ebe9bce5bb97417341dea30680e7669eceefba41
Eliot Horowitz <eliot@10gen.com>
2008-08-24 20:54:25 -0400

    sort/skip test

--- src/test/ed/db/sort1.js
+++ src/test/ed/db/sort1.js
@@ -14,6 +14,8 @@ for( var pass = 0; pass < 2; pass++ ) {
     assert( t.find().sort({x:1})[0].x == 1 );
     assert( t.find().sort({x:-1})[0].x == 5 );
     assert( t.find().sort({x:-1})[1].x == 3 );
+    assert.eq( t.find().sort({x:-1}).skip(0)[0].x , 5 );
+    assert.eq( t.find().sort({x:-1}).skip(1)[0].x , 3 );
     t.ensureIndex({x:1});
 
 }
