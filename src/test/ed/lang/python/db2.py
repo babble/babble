2ce75916f5585b1d0f2ca9d9cbea2b5c3ba45afb
Eliot Horowitz <eliot@10gen.com>
2008-08-26 16:46:07 -0400

    broken unit test

--- src/test/ed/lang/python/db2.py
+++ src/test/ed/lang/python/db2.py
@@ -8,3 +8,5 @@ t.save( { "name" : "eliot" , "foo" : [ 1 , 2 ] } );
 x = t.findOne();
 _10gen.assert.eq( "eliot" , x.name );
 _10gen.assert.eq( 2 , len( x.foo ) );
+_10gen.assert.eq( 1 , x.foo[0] );
+_10gen.assert.eq( 2 , x.foo[1] );
