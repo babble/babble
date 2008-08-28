680e6cefde733606479ace535794f1afa0316b34
dwight <dwight@Dwights-MacBook.local>
2008-08-28 09:58:12 -0400

    minor test enhancement

--- src/test/ed/db/sort1.js
+++ src/test/ed/db/sort1.js
@@ -12,6 +12,7 @@ t.save({x:1,z:33});
 
 for( var pass = 0; pass < 2; pass++ ) {
     assert( t.find().sort({x:1})[0].x == 1 );
+    assert( t.find().sort({x:1}).skip(1)[0].x == 2 );
     assert( t.find().sort({x:-1})[0].x == 5 );
     assert( t.find().sort({x:-1})[1].x == 3 );
     t.ensureIndex({x:1});
