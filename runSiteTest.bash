ae0308da39237df86dc0ab876aaa1c4a2667dd0f
Jim Menard <jim@10gen.com>
2008-09-03 12:05:55 -0400

    Added Ruby std libs. Give jruby.home value to runtime in startup scripts.

--- runSiteTest.bash
+++ runSiteTest.bash
@@ -12,6 +12,7 @@ export headless="-Djava.awt.headless=true"
 #if [ $?gui ]; then
     #export headless="" #was having issue so commented this out
 #fi
+export jruby_home="-Djruby.home=`dirname $0`/build/ruby"
 
-ant && java -Djava.library.path=include -ea $headless -Xmx1000m  -XX:MaxDirectMemorySize=600M "$@"
+ant && java -Djava.library.path=include -ea $headless $jruby_home -Xmx1000m  -XX:MaxDirectMemorySize=600M "$@"
 
