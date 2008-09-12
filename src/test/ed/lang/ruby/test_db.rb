17a624589ea9ca82103133f4ee8ae1ee9fe5219f
Jim Menard <jim@10gen.com>
2008-09-12 10:43:41 -0400

    Added copyright notices. Removed unnecessary comment.

--- src/test/ed/lang/ruby/test_db.rb
+++ src/test/ed/lang/ruby/test_db.rb
@@ -1,3 +1,17 @@
+# Copyright (C) 2008 10gen Inc.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License, version 3, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 require 'ruby_test'
 
 class DBTest < RubyTest
@@ -17,11 +31,7 @@ EOS
   end
 
   def teardown
-    # AHA! is this showing that db is not defined?
-    run_js <<EOS
-print("db = " + db); // DEBUG
-db.rubytest.remove({});
-EOS
+    run_js 'db.rubytest.remove({});'
     super
   end
 
