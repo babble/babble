c925133c1dca027e89ec35e026eda038f3818202
Jim Menard <jim@10gen.com>
2008-09-12 10:59:57 -0400

    Moved a bunch of tests from Java to Ruby.

--- src/test/ed/lang/ruby/test_db.rb
+++ src/test/ed/lang/ruby/test_db.rb
@@ -28,6 +28,8 @@ db.rubytest.save({artist: 'XTC', album: 'Oranges & Lemons', song: 'Garden Of Ear
 song_id = db.rubytest.save({artist: 'XTC', album: 'Oranges & Lemons', song: 'The Mayor Of Simpleton', track: 2});
 db.rubytest.save({artist: 'XTC', album: 'Oranges & Lemons', song: 'King For A Day', track: 3});
 EOS
+    @song_id = $song_id._id
+    @coll = $db.rubytest
   end
 
   def teardown
@@ -45,10 +47,53 @@ EOS
   end
 
   def test_collection_wrapper
-    coll = $db.rubytest
-    assert_not_nil(coll)
-    assert coll.respond_to?(:findOne)
-    assert coll.respond_to?(:find)
+    assert_not_nil(@coll)
+    assert @coll.respond_to?(:findOne)
+    assert @coll.respond_to?(:find)
+  end
+
+  def test_find_any_one
+    json = tojson($db.rubytest.findOne())
+    assert(json =~ /artist/, "JSON representation missing 'artist': #{json}")
+  end
+
+  def find_one_by_object_id
+    assert_simpleton(@coll.findOne($song_id))
+  end
+
+  def find_one_by_id_using_string
+    assert_simpleton(@coll.findOne(@song_id))
+  end
+
+  def test_find_one_by_id_using_hash
+    assert_simpleton(@coll.findOne({:_id => @song_id}))
+  end
+
+  def test_find_one_by_song
+    assert_simpleton(@coll.findOne({:song => 'The Mayor Of Simpleton'}))
+  end
+
+  def test_find
+    assert_all_rows(@coll.find.inject('') {|str, row| str + tojson(row)})
+  end
+
+  def assert_simpleton(x)
+    assert_not_nil(x)
+    assert_equal(@song_id, x._id)
+    assert_equal("XTC", x.artist)
+    assert_equal("Oranges & Lemons", x.album)
+    assert_equal("The Mayor Of Simpleton", x.song)
+    assert_not_nil(x.track)
+    assert_equal(2, x.track.to_i)
+  end
+
+  def assert_all_rows(str)
+    assert str =~ /Swing/
+    assert str =~ /Blimp/
+    assert str =~ /Pirate/
+    assert str =~ /Garden/
+    assert str =~ /Simpleton/
+    assert str =~ /King/
   end
 
 end
