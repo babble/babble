0c11221de58c6e99c4c73ca7147ed26d39fae5e1
Jim Menard <jim@10gen.com>
2008-09-15 17:01:46 -0400

    Refactored the *.bash, mongo, and shell scripts, creating one common file that all of them.

--- runCygwin.bash
+++ runCygwin.bash
@@ -1,27 +1,4 @@
 #!/bin/bash
 
-
-
-export TZ=America/New_York
-
-export CLASSPATH=".;build;conf"
-for j in `ls include/*.jar`; do
-    export CLASSPATH="$CLASSPATH;$j"
-done
-
-for j in `ls include/jython/*.jar`; do
-    export CLASSPATH="$CLASSPATH;$j"
-done
-
-for j in `ls include/jython/javalib/*.jar`; do
-    export CLASSPATH="$CLASSPATH;$j"
-done
-
-
-export CLASSPATH=$CLASSPATH:/opt/java/lib/tools.jar
-
-export headless="-Djava.awt.headless=true"
-export jruby_home="-Djruby.home=`dirname $0`/include/ruby"
-
-java -ea -Djava.library.path=include $headless $jruby_home -ea -Xmx1000m -XX:MaxDirectMemorySize=600M "$@"
-
+. `dirname $0`/common.bash
+$java_memory_large "$@"
