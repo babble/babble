d1fd2555ac6562f5903c4152e338bb442f471984
Eliot Horowitz <eliot@10gen.com>
2008-09-16 18:36:45 -0400

    apparently the eclipse compilation universe is actually thread safe start pooling them in a hope of being able to cache jar files in the future

--- src/main/ed/js/engine/CompileUtil.java
+++ src/main/ed/js/engine/CompileUtil.java
@@ -26,6 +26,7 @@ import org.eclipse.jdt.internal.compiler.batch.*;
 
 import ed.io.*;
 import ed.db.*;
+import ed.util.*;
 
 public class CompileUtil {
     
@@ -52,68 +53,64 @@ public class CompileUtil {
         return TMP_DIR + pack.replace( '.' , '/' ) + "/";
     }
     
-    public static synchronized Class<?> compile( final String pack , final String c , final String source , final Convert convert )
+    public static Class<?> compile( final String pack , final String c , final String source , final Convert convert )
         throws IOException , ClassNotFoundException {
         
         final boolean D = convert.D;
 
         if ( CD ) System.err.println( "compile called" );
         if ( D ) System.out.println( source );
-        if ( CD ) System.err.println( "compile 0 " );        
-
+        
         File dir = new File( getCompileSrcDir( pack ) );
-        dir.mkdirs();
-
-        if ( CD ) System.err.println( "compile 1 " );
-
+        synchronized ( _globalLock ){
+            dir.mkdirs();
+        }
+        
         File f = new File( dir , c + ".java" );
         File output = new File( f.getAbsolutePath().replaceAll( "java$" , "class" ) );
-        long depend = getDependencyLastTime();
 
-        if ( CD ) System.err.println( "compile 2 " );
-
-        String old = null;
-        if ( f.exists() )
-            old = StreamUtil.readFully( new FileInputStream( f ) );
-
-        boolean oldOK = source.equals( old ) && output.exists() && output.lastModified() > depend;
-        if ( CD ) System.err.println( "compile 3 " );
-
-        if ( ! oldOK ){
-        
-            FileOutputStream fout = new FileOutputStream( f );
-            fout.write( source.getBytes() );
-            fout.close();
-
-            if ( CD ) System.err.println( "compile 3.1 " );
+        synchronized( output.toString().intern() ){
+            long depend = getDependencyLastTime();
             
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter( sw ); 
+            String old = null;
+            if ( f.exists() )
+                old = StreamUtil.readFully( new FileInputStream( f ) );
             
-            if ( CD ) System.err.println( "compile 3.2 " );
-            if ( CD ) System.err.println( "compile 3.2.1 " );
-            //int res = com.sun.tools.javac.Main.compile( new String[]{ "-g" , f.toString() } , pw );
+            boolean oldOK = source.equals( old ) && output.exists() && output.lastModified() > depend;
             
-            String cp = "build";
-            if ( JSHook.whereIsEd != null )
-                cp += File.pathSeparatorChar + JSHook.whereIsEd + "/build";
-            boolean res = org.eclipse.jdt.internal.compiler.batch.Main.compile( "-g -1.5 -classpath " + cp + " " + f.toString() , pw , pw );
-            if ( D ) System.out.println( f + " : " + res );
-            
-            if ( CD ) System.err.println( "compile 3.3 " );
+            if ( ! oldOK ){
+                
+                FileOutputStream fout = new FileOutputStream( f );
+                fout.write( source.getBytes() );
+                fout.close();
+                
+                if ( CD ) System.err.println( "going to start compiling " );
+                
+                String cp = "build";
+                if ( JSHook.whereIsEd != null )
+                    cp += File.pathSeparatorChar + JSHook.whereIsEd + "/build";
 
-            if ( ! res ){
-                System.err.println( "**" + sw );
-                throw new RuntimeException( sw.toString() );
+                MyCompiler compiler = _compilerPool.get();
+                boolean res = false;
+                try {
+                    res = compiler.compile( "-g -1.5 -classpath " + cp + " " + f.toString() );
+                }
+                finally {
+                    _compilerPool.done( compiler );
+                }
+                
+                if ( D ) System.out.println( f + " : " + res );
+                if ( CD ) System.err.println( "done compiling " );
+                
+                if ( ! res ){
+                    System.err.println( "**" + compiler.getLastMessage() );
+                    throw new RuntimeException( compiler.getLastMessage() );
+                }
+                
             }
-
-            if ( CD ) System.err.println( "compile 3.4 " );
-
-        }
-
-        if ( CD ) System.err.println( "compile 4 " );
             
-        return _loader.loadClass( pack + "." + c );
+            return _loader.loadClass( pack + "." + c );
+        }
     }
 
     static long getDependencyLastTime(){
@@ -126,14 +123,55 @@ public class CompileUtil {
         }
         return max;
     }
+
+    static class MyCompiler {
+        
+        MyCompiler(){
+            _output = new StringWriter();
+            PrintWriter pw = new PrintWriter( _output );
+            _main = new Main( pw , pw , false );
+        }
+
+        boolean compile( String commandLine ){
+            _output.getBuffer().setLength( 0 );
+            _lastRes = _main.compile( _main.tokenize( commandLine ) );
+            _lastMessage = _output.toString();
+            return _lastRes;
+        }
+        
+        boolean ok(){
+            return _lastRes;
+        }
+        
+        String getLastMessage(){
+            return _lastMessage;
+        }
+
+        final StringWriter _output;
+        final Main _main;
+
+        boolean _lastRes = true;
+        String _lastMessage = null;
+    }
     
-    private static Set<File> _dependFiles = new HashSet<File>();
-    private static String _possibleRoots[] = new String[]{ 
+    private static final SimplePool<MyCompiler> _compilerPool = new SimplePool<MyCompiler>( "MyCompiler" , 4 , 4 ){
+        
+        public MyCompiler createNew(){
+            return new MyCompiler();
+        }
+
+        public boolean ok( MyCompiler c ){
+            return c.ok();
+        }
+    };
+    private static final String _globalLock = "CompileUtil-GlobalLock-123";
+    private static final Set<File> _dependFiles = new HashSet<File>();
+    private static final String _possibleRoots[] = new String[]{ 
         "" ,
         "../ed" ,
         "../../ed" ,
     };
-    private static String _dependsDirs[] = new String[]{ 
+    private static final String _dependsDirs[] = new String[]{ 
         "src/main/ed/js" , 
         "src/main/ed/js/engine" , 
         "src/main/ed/js/func" };
