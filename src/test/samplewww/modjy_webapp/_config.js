614cad69fd7fda1479f8c394407acacbcdc84e0a
Ethan Glasser-Camp <ethan@10gen.com>
2008-09-18 09:51:34 -0400

    Importing ModjyServlet as ModJyConnector.
    Non-functional change; just setting things up so that I can start
    to interfere with mod_jy.

--- src/test/samplewww/modjy_webapp/_config.js
+++ src/test/samplewww/modjy_webapp/_config.js
@@ -1,5 +1,6 @@
 
 servletConfigs = {};
-servletConfigs["com.xhaus.modjy.ModjyJServlet"] = {};
-servletConfigs["com.xhaus.modjy.ModjyJServlet"]["app_filename"] = "__testing__.py";
-servletConfigs["com.xhaus.modjy.ModjyJServlet"]["modjy_jar.location"] = "include/modjy.jar";
+var py = 'ed.lang.python.ModJyConnector';
+servletConfigs[py] = {};
+servletConfigs[py]["app_filename"] = "__testing__.py";
+servletConfigs[py]["modjy_jar.location"] = "include/modjy.jar";
