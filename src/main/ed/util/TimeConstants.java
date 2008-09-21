8a5fbbe1fcdde30d200dac864db4c27b4604974e
Eliot Horowitz <eliot@10gen.com>
2008-09-21 00:00:42 -0400

    some time constants

---
+++ src/main/ed/util/TimeConstants.java
@@ -0,0 +1,24 @@
+// TimeConstants.java
+
+package ed.util;
+
+public class TimeConstants {
+    
+    public static final long MS_MILLISECOND = 1;
+    public static final long MS_SECOND = 1000;
+    public static final long MS_MINUTE = MS_SECOND * 60;
+    public static final long MS_HOUR = MS_MINUTE * 60;
+    public static final long MS_DAY = MS_HOUR * 24;
+    public static final long MS_WEEK = MS_DAY * 7;    
+    public static final long MS_MONTH = MS_WEEK * 4;    
+    public static final long MS_YEAR = MS_DAY * 365;
+
+    public static final long S_SECOND = 1;
+    public static final long S_MINUTE = 60 * S_SECOND;
+    public static final long S_HOUR = 60 * S_MINUTE;
+    public static final long S_DAY = 24 * S_HOUR;
+    public static final long S_WEEK = 7 * S_DAY;
+    public static final long S_MONTH = 30 * S_DAY;
+    public static final long S_YEAR = S_DAY * 365;
+}
+
