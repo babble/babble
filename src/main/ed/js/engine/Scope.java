755ffd7d3da9c13f56673cc4b10605f763fb5303
kristina <k@wooster.earthlink.net>
2008-09-22 11:39:05 -0400

    shell prints objects of the form x.y correctly

--- src/main/ed/js/engine/Scope.java
+++ src/main/ed/js/engine/Scope.java
@@ -716,8 +716,13 @@ public final class Scope implements JSObject , Bindings {
             if ( code.matches( "\\d+" ) )
                 return Integer.parseInt( code );
 
-            if ( code.matches( "\\w+(\\.\\w+)*" ) )
-                return findObject( code );
+            if ( code.matches( "\\w+(\\.\\w+)*" ) ) {
+                Object o = findObject( code );
+                if( hasReturn != null && hasReturn.length > 0 ) {
+                    hasReturn[0] = ( o == null ) ? false : true;
+                }
+                return o;
+            }
             
             // tell the Convert CTOR that we're in the context of eval so
             //  not use a private scope for the execution of this code
