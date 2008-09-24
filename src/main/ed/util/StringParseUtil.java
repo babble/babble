9b233c049c05da8f769ee02818dca15f454e579b
kristina <k@wooster.earthlink.net>
2008-09-24 13:13:19 -0400

    added Math functions/fields

--- src/main/ed/util/StringParseUtil.java
+++ src/main/ed/util/StringParseUtil.java
@@ -204,7 +204,15 @@ public final class StringParseUtil {
      * @return the numeric value
      */
     public static Number parseStrict( String s ){
-        if( s.indexOf('.') != -1 )
+        if( s.matches( "-?Infinity" ) ) {
+            if( s.startsWith( "-" ) ) {
+                return Double.NEGATIVE_INFINITY;
+            }
+            else {
+                return Double.POSITIVE_INFINITY;
+            }
+        }
+        else if( s.indexOf('.') != -1 )
             return Double.parseDouble(s);
         else if( s.length() > 2 && s.charAt( 0 ) == '0' && s.charAt( 1 ) == 'x')
             return Integer.parseInt( s.substring( 2, s.length() ) , 16 );
