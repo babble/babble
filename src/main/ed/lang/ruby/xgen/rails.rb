24ef88a8cd502aee2e2f0e8a800012bcf56e1123
Jim Menard <jim@10gen.com>
2008-09-26 14:56:15 -0400

    updated _init.rb example

--- src/main/ed/lang/ruby/xgen/rails.rb
+++ src/main/ed/lang/ruby/xgen/rails.rb
@@ -22,7 +22,7 @@ looks like this:
 # to the 10gen Rails dispatcher.
 $mapUrlToJxpFile = Proc.new do |uri, req|
   uri = '/index.html' if uri == '/'
-  if File.exist?(File.join(File.dirname(__FILE__), 'public', uri[1..-1]))
+  if File.exist?(File.join($local.getRoot.getPath, 'public', uri[1..-1]))
     "/public" + uri
   else
     "public/xgen_dispatch.rb"
