ee67b15829c5c6a9e8ebc6036a97a190246817e7
kristina <k@wooster.earthlink.net>
2008-09-26 09:24:58 -0400

    added Boolean type

--- src/main/ed/js/JSBoolean.java
+++ src/main/ed/js/JSBoolean.java
@@ -24,15 +24,95 @@ import ed.util.*;
 import ed.js.func.*;
 import ed.js.engine.*;
 
-public class JSBoolean {
+public class JSBoolean extends JSObjectBase {
 
+    public static JSFunction _cons = new Cons();
 
-    public static JSObjectBase functions = new JSObjectBase();
-    public static JSFunction getFunction( String name ){
-        return (JSFunction)functions.get( name );
+    public static class Cons extends JSFunctionCalls1{
+
+        public JSObject newOne(){
+            return new JSBoolean();
+        }
+
+        public Object call( Scope scope , Object a , Object[] extra ){
+            Object foo = scope.getThis();
+
+            JSBoolean e;
+            if( foo instanceof JSBoolean ) {
+                e = (JSBoolean)foo;
+                e.init( a );
+            }
+            else {
+                e = new JSBoolean( a );
+            }
+            return e.value;
+        }
+
+        protected void init(){
+            _prototype.set( "valueOf" , new JSFunctionCalls0() {
+                    public Object call( Scope s , Object foo[] ){
+                        Object o = s.getThis();
+                        if( !(o instanceof JSBoolean ) )
+                            throw new JSException( "error" );
+
+                        return ((JSBoolean)o).value;
+                    }
+                } );
+            _prototype.set( "toString" , new JSFunctionCalls0() {
+                    public Object call( Scope s , Object foo[] ) {
+                        Object o = s.getThis();
+                        if( !(o instanceof JSBoolean ) )
+                            throw new JSException( "error" );
+
+                        return ((JSBoolean)o).value + "";
+                    }
+                } );
+
+            _prototype.dontEnumExisting();
+            dontEnum( "prototype" );
+        }
     }
 
-    static {
+    public void init( Object o ) {
+        value = JSBoolean.booleanValue( o );
+    }
 
+    public JSBoolean() {
+        this( null );
+    }
+
+    public JSBoolean( Object o ) {
+        super( _getCons() );
+        init( o );
+    }
+
+    public boolean value = false;
+
+    private static JSFunction _getCons(){
+        return Scope.getThreadLocalFunction( "Boolean" , _cons );
+    }
+
+    public static boolean booleanValue( Object o ) {
+        if( o == null )
+            return false;
+
+        if( o instanceof JSBoolean ) {
+            return ((JSBoolean)o).value;
+        }
+        else if ( o instanceof Boolean ) {
+            return ((Boolean)o).booleanValue();
+        }
+
+        if ( o instanceof JSString ||
+             o instanceof String ) {
+            return o.toString().length() > 0;
+        }
+
+        if ( o instanceof Number ) {
+            double d = ((Number)o).doubleValue();
+            return d == 1 || d == -1 || Double.isInfinite( d );
+        }
+
+        return false;
     }
 }
