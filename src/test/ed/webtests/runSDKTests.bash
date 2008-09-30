db787386df4d8fa7a8c0125411fbf0831727859e
Mike Dirolf <mike@10gen.com>
2008-09-30 15:05:41 -0400

    sleep much longer. hopefully give Jython jars time to load.

--- src/test/ed/webtests/runSDKTests.bash
+++ src/test/ed/webtests/runSDKTests.bash
@@ -51,7 +51,7 @@ for site in $SITES_LIST; do
     # Bring up the app server.
     ./runserver.sh ./sites/$site&
 
-	sleep 5
+	sleep 45
 
     # Populate the db with setup data.
     if [ -f ./sites/${site}test/setup.js ]
