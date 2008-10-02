bf19c7f90e39f55dcfa4c6bd05ceb561af5f10e8
Mike Dirolf <mike@10gen.com>
2008-10-02 17:01:53 -0400

    remove trailing whitespace

--- src/main/ed/lang/python/Python.java
+++ src/main/ed/lang/python/Python.java
@@ -2,16 +2,16 @@
 
 /**
 *    Copyright (C) 2008 10gen Inc.
-*  
+*
 *    This program is free software: you can redistribute it and/or  modify
 *    it under the terms of the GNU Affero General Public License, version 3,
 *    as published by the Free Software Foundation.
-*  
+*
 *    This program is distributed in the hope that it will be useful,
 *    but WITHOUT ANY WARRANTY; without even the implied warranty of
 *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *    GNU Affero General Public License for more details.
-*  
+*
 *    You should have received a copy of the GNU Affero General Public License
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -47,7 +47,7 @@ public class Python extends Language {
         Options.includeJavaStackInExceptions = true;
         PySystemState.initialize();
     }
-    
+
     public static PyCode compile( File f )
         throws IOException {
         return (PyCode)(Py.compile( new FileInputStream( f ) , f.toString() , "exec" ));
@@ -71,9 +71,9 @@ public class Python extends Language {
 
         if ( p == null || p instanceof PyNone )
             return null;
- 
+
         if ( p instanceof JSObject ||
-             p instanceof JSString || 
+             p instanceof JSString ||
              p instanceof Number )
             return p;
 
@@ -94,10 +94,10 @@ public class Python extends Language {
 
         if ( p instanceof PyInteger )
             return ((PyInteger)p).getValue();
-        
+
         if ( p instanceof PyFloat )
             return ((PyFloat)p).getValue();
-        
+
         if ( p instanceof PyString )
             return new JSString( p.toString() );
 
@@ -116,15 +116,15 @@ public class Python extends Language {
         if ( p instanceof PyObject )
             return new JSPyObjectWrapper( (PyObject)p );
 
-        throw new RuntimeException( "can't convert [" + p.getClass().getName() + "] from py to js" );       
+        throw new RuntimeException( "can't convert [" + p.getClass().getName() + "] from py to js" );
     }
-    
+
     public static PyObject toPython( Object o ){
         return toPython( o , null );
     }
 
     public static PyObject toPython( Object o , Object useThis ){
-        
+
         if ( o == null )
             return Py.None;
 
@@ -136,22 +136,22 @@ public class Python extends Language {
 
         if ( o instanceof PyObject )
             return (PyObject)o;
-        
+
         if ( o instanceof Boolean )
             return new PyBoolean( (Boolean)o );
 
         if ( o instanceof Integer )
             return new PyInteger( ((Integer)o).intValue() );
-        
+
         if ( o instanceof Number )
             return new PyFloat( ((Number)o).doubleValue() );
-        
+
         if ( o instanceof String )
             return new PyString( (String)o );
 
         if ( o instanceof JSString )
             return new PyJSStringWrapper( (JSString)o );
-        
+
         if ( o instanceof ed.db.ObjectId )
             return new PyObjectId( (ed.db.ObjectId)o );
 
@@ -176,11 +176,11 @@ public class Python extends Language {
 
             return new PyJSFunctionWrapper( (JSFunction)o , useThis );
         }
-        
+
 
         if ( o instanceof JSObject )
             return new PyJSObjectWrapper( (JSObject)o );
-        
+
         throw new RuntimeException( "can't convert [" + o.getClass().getName() + "] from js to py" );
     }
 
@@ -239,7 +239,7 @@ public class Python extends Language {
     }
 
     public static JSFunction extractLambda( final String source ){
-        
+
         final PyCode code = (PyCode)(Py.compile( new ByteArrayInputStream( source.getBytes() ) , "anon" , "exec" ) );
 
         if ( _extractGlobals == null )
@@ -247,35 +247,35 @@ public class Python extends Language {
 
         Scope s = _extractGlobals.child();
         s.setGlobal( true );
-        PyObject globals = new PyJSScopeWrapper( s , false );        
+        PyObject globals = new PyJSScopeWrapper( s , false );
 
         PyModule module = new PyModule( "__main__" , globals );
         PyObject locals = module.__dict__;
-        
+
         Set<String> before = new HashSet<String>( s.keySet() );
         Py.runCode( code, locals, globals );
         Set<String> added = new HashSet<String>( s.keySet() );
         added.removeAll( before );
-        
+
         JSPyObjectWrapper theFunc = null;
-        
+
         for ( String n : added ){
             if ( s.get( n ) == null )
                 continue;
             Object foo = s.get( n );
             if ( ! ( foo instanceof JSPyObjectWrapper ) )
                 continue;
-            
+
             JSPyObjectWrapper p = (JSPyObjectWrapper)foo;
             if ( ! p.isCallable() )
                 continue;
-            
+
             if ( p.getPyCode() == null )
                 continue;
-            
+
             theFunc = p;
         }
-        
+
         return new JSPyObjectWrapper( (PyFunction)(theFunc.getContained()) , true );
     }
 
