80320c1c3f080fbe66bfc2c64334e73c1fd17ac1
Jim Menard <jim@10gen.com>
2008-10-07 14:23:36 -0400

    Fixed find-by-id problems. Introduced an ObjectId wrapper class and an in-Ruby ObjectId method that returns a "real" wrapped ObjectId.

--- src/test/ed/lang/ruby/test_db.rb
+++ src/test/ed/lang/ruby/test_db.rb
@@ -25,10 +25,10 @@ db.rubytest.save({artist: 'Thomas Dolby', album: 'Aliens Ate My Buick', song: 'T
 db.rubytest.save({artist: 'Thomas Dolby', album: 'Aliens Ate My Buick', song: 'Budapest by Blimp'});
 db.rubytest.save({artist: 'Thomas Dolby', album: 'The Golden Age of Wireless', song: 'Europa and the Pirate Twins'});
 db.rubytest.save({artist: 'XTC', album: 'Oranges & Lemons', song: 'Garden Of Earthly Delights', track: 1});
-song_id = db.rubytest.save({artist: 'XTC', album: 'Oranges & Lemons', song: 'The Mayor Of Simpleton', track: 2});
+track = db.rubytest.save({artist: 'XTC', album: 'Oranges & Lemons', song: 'The Mayor Of Simpleton', track: 2});
 db.rubytest.save({artist: 'XTC', album: 'Oranges & Lemons', song: 'King For A Day', track: 3});
 EOS
-    @song_id = $song_id._id
+    @song_id = $track._id.to_s
     @coll = $db.rubytest
   end
 
@@ -42,8 +42,8 @@ EOS
   end
 
   def test_song_id_exists
-    run_js 'x = song_id._id.toString()'
-    assert_equal($song_id._id, $x)
+    run_js 'x = track._id.toString()'
+    assert_equal($track._id.to_s, $x)
   end
 
   def test_collection_wrapper
@@ -58,7 +58,7 @@ EOS
   end
 
   def find_one_by_object_id
-    assert_simpleton(@coll.findOne($song_id))
+    assert_simpleton(@coll.findOne($track._id))
   end
 
   def find_one_by_id_using_string
@@ -108,7 +108,7 @@ EOS
 
   def assert_simpleton(x)
     assert_not_nil(x)
-    assert_equal(@song_id, x._id)
+    assert_equal(@song_id, x._id.to_s) # x.id returns string, x._id returns ObjectId
     assert_equal("XTC", x.artist)
     assert_equal("Oranges & Lemons", x.album)
     assert_equal("The Mayor Of Simpleton", x.song)
