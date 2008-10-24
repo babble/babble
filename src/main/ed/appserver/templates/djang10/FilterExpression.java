73781a2f468d64a714b85ee86ced2d9391a798c6
Igor Bernstein <igor@10gen.com>
2008-10-24 16:21:53 -0400

    move all expression evaling logging to debug level

--- src/main/ed/appserver/templates/djang10/FilterExpression.java
+++ src/main/ed/appserver/templates/djang10/FilterExpression.java
@@ -94,21 +94,7 @@ public class FilterExpression extends JSObjectBase {
             }
             else {
                 if(!e.isSameAsJsNull()) {
-                    if(log.getEffectiveLevel().compareTo(Level.DEBUG) <= 0) {
-                        log.info(e.getMessage(),e);
-                    } 
-                    else {
-                        Throwable t=e;
-                        StringBuilder buffer = new StringBuilder();
-                        while(t != null) {
-                            if(t != e)
-                                buffer.append("Caused By: ");
-                                
-                            buffer.append(t.getMessage());
-                            t = t.getCause();
-                        }
-                        log.info(buffer);
-                    }
+                    log.debug(e.getMessage(), e);
                 }
                 
                 JSHelper jsHelper = JSHelper.get(scope);
