9912b71f8528eb41508903d69b52117f1da593f3
Dwight <dmerriman@gmail.com>
2008-11-03 20:47:30 -0500

    new test

--- src/main/ed/db/db.js
+++ src/main/ed/db/db.js
@@ -62,12 +62,12 @@ DB.prototype.dropDatabase = function() {
 
 /**
   Clone database on another server to here.
-
+  <p>
   Generally, you should dropDatabase() first as otherwise the cloned information will MERGE 
   into whatever data is already present in this database.  (That is however a valid way to use 
   clone if you are trying to do something intentionally, such as union three non-overlapping
   databases into one.)
- 
+  <p>
   This is a low level administrative function will is not typically used.
 
  * @param {String} from Where to clone from (dbhostname[:port]).  May not be this database 
