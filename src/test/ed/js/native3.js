2c291725a0acf4dddc76d230b0a776c48eb16032
Eliot Horowitz <eliot@10gen.com>
2008-11-10 23:09:05 -0500

    fix for java6

--- src/test/ed/js/native3.js
+++ src/test/ed/js/native3.js
@@ -1,6 +1,6 @@
 
 a = [ 1 , 2 , 3 ];
-assert.eq( "[1.0, 2.0, 3.0]" , javaStatic( "java.util.Arrays" , "toString" , a ) );
+assert.eq( "[1, 2, 3]" , javaStatic( "java.util.Arrays" , "toString" , a ).toString().replace( /\.\d+/g , "" ) );
 
 assert.eq( 2 , javaStatic( "ed.js.NativeHelp1" , "count" , [ javaCreate( "ed.js.NativeHelp1" ) , javaCreate( "ed.js.NativeHelp1" ) ] ) );
 
