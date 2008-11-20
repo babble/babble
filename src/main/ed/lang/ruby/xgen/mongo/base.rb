07eb8ec10feeed9cbf3f7bc9b1a31b529e7ff7d1
Jim Menard <jim@10gen.com>
2008-11-20 15:25:08 -0500

    Consolidated convertion to Mongo objects into one file, from X:M:B and ActiveRecord.

--- src/main/ed/lang/ruby/xgen/mongo/base.rb
+++ src/main/ed/lang/ruby/xgen/mongo/base.rb
@@ -14,30 +14,8 @@
 
 require 'xgen/mongo/oid'
 require 'xgen/mongo/cursor'
+require 'xgen/mongo/convert'
 require 'xgen/sql'
-
-class Object
-  def to_mongo_value
-    self
-  end
-end
-class Array
-  def to_mongo_value
-    self.collect {|v| v.to_mongo_value}
-  end
-end
-class JSFunction
-  def to_mongo_value
-    self
-  end
-end
-class Hash
-  def to_mongo_value
-    h = {}
-    self.each {|k,v| h[k] = v.to_mongo_value}
-    h
-  end
-end
 
 module XGen
 
