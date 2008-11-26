1fda70ecd7ae0329c2395e9b7c6da596af2eb241
Eliot Horowitz <eliot@10gen.com>
2008-11-25 20:06:57 -0500

    some simple refactoring

--- src/main/ed/security/AppSecurityManager.java
+++ src/main/ed/security/AppSecurityManager.java
@@ -39,51 +39,8 @@ public final class AppSecurityManager extends SecurityManager {
     }
 
     public AppSecurityManager(){
-        _os = Machine.getOSType();
         _logger = Logger.getLogger( "security" );
-        _javaRoot = (new File(".")).getAbsolutePath().replaceAll( "\\.$" , "" );
-
-        List<String> okRead = new ArrayList<String>();
-        List<String> okWrite = new ArrayList<String>();
-
-        okRead.add( _javaRoot );
-        okRead.add( "include" );
-        okRead.add( "build" );
-        okRead.add( "conf" );
-        okRead.add( "src" );
-        okRead.add( "." );
-        okRead.add( "/opt/java/" );
-        okWrite.add( "logs/" );        
-
-        okRead.add( System.getProperty( "user.home" ) + "/.jython" );
-        
-        okWrite.add( WorkingFiles.getTypeDir( "jython-cache" ).getAbsolutePath() );
-
-        
-        if ( _os.isMac() ){
-            okWrite.add( "/private" + WorkingFiles.getTypeDir( "jython-cache" ).getAbsolutePath() );
-            okRead.add( "/System/" );
-            okRead.add( "/Library/" );
-        }
-        else if ( _os.isLinux() ){
-            okRead.add( "/usr/" );
-        }
-
-        System.out.println( "okRead : " + okRead );
-        System.out.println( "okWrite : " + okWrite );
-
-        _okRead = new String[okRead.size()];
-        okRead.toArray( _okRead );
-
-        _okWrite = new String[okWrite.size()];
-        okWrite.toArray( _okWrite );
-
-        _jsCompileRoot = CompileUtil.getCompileSrcDir( Convert.DEFAULT_PACKAGE ).replaceAll( "/+$" , "" );
-
-
-        _publicClasses = new String[_publicDirs.length];
-        for ( int i=0; i<_publicDirs.length; i++ )
-            _publicClasses[i] = Convert.cleanName( _publicDirs[i] );
+        _file = new FileSecurity();
     }
 
     public void checkPermission(Permission perm) {
@@ -104,81 +61,24 @@ public final class AppSecurityManager extends SecurityManager {
             return;
         
         final AppContext ctxt = ar.getContext();
-
+        
         final String file = fp.getName();
         final String action = fp.getActions();
         final boolean read = action.equals( "read" );
-        final String ctxtRoot = ctxt.getRoot();
-
-        if ( read )
-            for ( int i=0; i<_okRead.length; i++ )
-                if ( file.startsWith( _okRead[i] ) )
-                    return;
-        
-        for ( int i=0; i<_okWrite.length; i++ )
-            if ( file.startsWith( _okWrite[i] ) )
-                return;
-        
-        for ( int i=0; i<_publicDirs.length; i++ )
-            if ( file.contains( _publicDirs[i] ) )
-                return;
-        
-        if ( file.startsWith( ctxtRoot ) )
+       
+        if ( _file.allowed( ctxt , file , read ) )
             return;
-        
-        if ( ctxtRoot.startsWith( file ) && 
-             ctxtRoot.length() - file.length() <= 1 && 
-             ctxtRoot.endsWith( "/" ) )
-            return;
-
-        if ( file.contains( ctxtRoot ) ){
-            if ( file.startsWith( _javaRoot + ctxtRoot ) )
-                return;
-        }
-        
-
+       
         final StackTraceElement topUser = ed.security.Security.getTopUserStackElement();
-        
-        if ( topUser == null )
-            return;
-
-        if ( file.startsWith( _jsCompileRoot ) ){
-            String wantSitePiece = file.substring( _jsCompileRoot.length() );
-            // this is basically just the dir
-            if ( wantSitePiece.length() < 3 )
-                return;
-            
-            while ( wantSitePiece.startsWith( "/" ) )
-                wantSitePiece = wantSitePiece.substring(1);
-            
-            // TODO - modules have to be a lot smarter
-            //        this is obviously full of holes
-            for ( int i=0; i<_publicClasses.length; i++ )
-                if ( wantSitePiece.contains( _publicClasses[i] ) )
-                    return;
-            
-            String goodSitePiece = Convert.cleanName( ctxtRoot );
-            if ( wantSitePiece.startsWith( goodSitePiece ) )
-                return;
-
-            System.err.println( "blah [" + wantSitePiece + "] [" + goodSitePiece + "]" );
-        }
-        
-        NotAllowed e = new NotAllowed( "not allowed to access [" + file + "] from [" + topUser + "] in site [" + ctxtRoot + "]" + fp , fp );
+              
+        NotAllowed e = new NotAllowed( "not allowed to access [" + file + "] from [" + topUser + "] in site [" + ctxt + "]" + fp , fp );
         e.fillInStackTrace();
         _logger.error( "invalid access [" + fp + "]" , e );
         throw e;
     }
     
-    final String[] _okRead;
-    final String[] _okWrite;
-    final Machine.OSType _os;
     final Logger _logger;
-    final String _jsCompileRoot;
-    final String _javaRoot;
-
-    final String[] _publicDirs = new String[]{ "/corejs/" , "/external/" , "/core-modules/" , "/site-modules/" , "src/main/ed/" }; // site-modules is special
-    final String[] _publicClasses;
+    final FileSecurity _file;
 
     static class NotAllowed extends AccessControlException implements StackTraceHolder.NoFix {
         NotAllowed( String msg , Permission p ){
