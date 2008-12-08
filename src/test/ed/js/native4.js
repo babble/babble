890329ed7378e61877fcc70f799948b53fcbaf3a
Eliot Horowitz <eliot@10gen.com>
2008-12-08 00:29:50 -0500

    unit test

---
+++ src/test/ed/js/native4.js
@@ -0,0 +1,4 @@
+
+assert.eq( 5 , javaStatic( "ed.js.NativeHelp1" , "num1" , 5 ) )
+assert.eq( 5 , javaStatic( "ed.js.NativeHelp1" , "num2" , 5 ) )
+assert.eq( 5 , javaStatic( "ed.js.NativeHelp1" , "num3" , 5 ) )
