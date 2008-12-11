4a1acdd168aaa3c6f0a3ec25f466bd81f92f9139
Ethan Glasser-Camp <ethan@10gen.com>
2008-12-11 16:17:04 -0500

    Actually implement this.

--- src/main/ed/lang/python/lib/thread.py
+++ src/main/ed/lang/python/lib/thread.py
@@ -1,6 +1,6 @@
 import _10gen
 from org.python.modules.thread.thread import LockType, __doc__, allocate_lock, \
-    exit, exit_thread, _local, error
+    exit, exit_thread, _local, error, get_ident
 
 def start_new_thread(func, args):
     _10gen.__instance__.queueWork( _generate_name(), func, *args )
@@ -8,8 +8,6 @@ def start_new_thread(func, args):
 _newFunctionThread = start_new_thread
 
 # interruptAllThreads is not exported
-def get_ident():
-    raise NotImplemented, 'get_ident not supported for 10gen threads'
 
 def stack_size():
     raise NotImplemented, 'stack_size not supported for 10gen threads'
