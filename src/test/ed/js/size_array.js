e4c8121f17b7311653b67849a441395b24bd6ca7
kristina <k@wooster.earthlink.net>
2008-12-12 15:39:03 -0500

    some sizing tests

--- src/test/ed/js/size_array.js
+++ src/test/ed/js/size_array.js
@@ -4,4 +4,33 @@ var before = arr.approxSize();
 
 arr.push("moooooooooooooooo");
 
-assert.gt(arr.approxSize(), before, "Array size didn't change");
+var size0 = arr.approxSize();
+assert.gt(size0, before, "Array size didn't change");
+
+// consistant?
+arr.push( "foo" );
+var size1 = arr.approxSize();
+arr.pop();
+var size2 = arr.approxSize();
+
+assert( size0 == size2 );
+assert( size0 < size1 );
+
+var rar = [1,2,3,4];
+size3 = rar.approxSize();
+
+arr = arr.concat( rar );
+assert( size0 < arr.approxSize() );
+assert( size3 + size0 > arr.approxSize() );
+
+// try adding a bunch of things
+var prevSize = rar.approxSize();
+for( var i=0; i<100; i++ ) {
+    rar.unshift( i );
+    var temp = rar.approxSize();
+    assert( temp > prevSize );
+    prevSize = temp;
+}
+
+// arrays are bigger than simple objs
+assert( [].approxSize() > {}.approxSize() );
