0ab72cfe930b6669aa05ac6e177897e811f9e4ba
Eliot Horowitz <eliot@10gen.com>
2008-12-26 18:11:48 -0500

    assignable

--- src/test/ed/cloud/_setup.js
+++ src/test/ed/cloud/_setup.js
@@ -1,5 +1,5 @@
-
 // setup.js
+
 assert( db );
 
 function checkDB( name , length ){
@@ -9,7 +9,7 @@ function checkDB( name , length ){
     assert.eq( length , db.dbs.find().toArray().length , "no [" + name + "] db, but lengths don't match for creating new one.  existing:"  + db.dbs.find().toArray().map( function(z){ return z.name; } ) );
     
     log( "adding fake db [" + name + "]" );
-    db.dbs.save( { name : name , type : "TEST" , server : "FAKE" } );
+    db.dbs.save( { name : name , type : "TEST" , server : "FAKE" , assignable : true } );
 }
 
 function checkPool( name , length ){
@@ -19,7 +19,7 @@ function checkPool( name , length ){
     assert.eq( length , db.pools.find().toArray().length , "no [" + name + "] pool, but lengths don't match for creating new one" );
 
     log( "adding fake pool [" + name + "]" );
-    db.pools.save( { name : name , envType : "TEST" , machines : [ "FAKE" ] } );
+    db.pools.save( { name : name , envType : "TEST" , machines : [ "FAKE" ]  , assignable : true } );
 }
 
 checkDB( "test1" , 0 );
