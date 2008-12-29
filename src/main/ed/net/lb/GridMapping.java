99cf20c617d985e317f72dd2c42145e92fb42eb2
Eliot Horowitz <eliot@10gen.com>
2008-12-28 23:17:52 -0500

    if no grid site, point to grid manager where it will be running

--- src/main/ed/net/lb/GridMapping.java
+++ src/main/ed/net/lb/GridMapping.java
@@ -129,6 +129,8 @@ public class GridMapping extends MappingBase {
             }
             
         }
+        
+        checkGridSite();
 
         String defaultPool = null;
 
@@ -160,6 +162,18 @@ public class GridMapping extends MappingBase {
 	    while ( i.hasNext() )
 		blockUrl( i.next().get( "url" ).toString() );
         
+    }
+
+    void checkGridSite(){
+
+        if ( getSiteInfo( "grid" , false ) != null )
+            return;
+        
+        _logger.info( "no grid site so adding to : " + _cloud.getGridLocation() );
+        for ( String s : _cloud.getGridLocation() )
+            addAddressToPool( "gridauto" , s );
+
+        addSiteMapping( "grid" , "www" , "gridauto" );
     }
 
     final Cloud _cloud;
