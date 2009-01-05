4561417f3145a95ccab04094e11e7a6b54c7c86f
Ethan Glasser-Camp <ethan@bowler.(none)>
2009-01-05 16:52:12 -0500

    Silence voluminous output.

--- src/test/ed/lang/python/thread2_test.py
+++ src/test/ed/lang/python/thread2_test.py
@@ -3,10 +3,18 @@ import ed.appserver.AppContext
 import ed.lang.python.Python
 import java.io.File
 
-# FIXME: this test produces a lot of output, due to prints in the
-# queueWork calls
 _10gen.__instance__ = ed.lang.python.Python.toPython(ed.appserver.AppContext(java.io.File('.')))
-_10gen.log.level = _10gen.log.LEVEL.ERROR
+
+import ed.log.Logger
+root = ed.log.Logger.getLogger('python')
+old_level = root.get('level')
+
+import ed.log.Level
+root.set('level', ed.log.Level.WARN)
 
 import test.test_thread
 import test.test_threading
+
+import time
+time.sleep(2)
+root.set('level', old_level)
